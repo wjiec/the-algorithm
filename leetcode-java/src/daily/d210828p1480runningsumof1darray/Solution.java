81fa240dfde2fe9d1a38af6d227c2cd8c5b531b5
Jayson <shadowman@shellboot.com>
2021-08-28 15:22:07 +0800

    daily

---
+++ leetcode-java/src/daily/d210828p1480runningsumof1darray/Solution.java
@@ -0,0 +1,30 @@
+package daily.d210828p1480runningsumof1darray;
+
+import common.Checker;
+
+/**
+ * 1480. Running Sum of 1d Array
+ *
+ * https://leetcode-cn.com/problems/running-sum-of-1d-array/
+ *
+ * Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ *
+ * Return the running sum of nums.
+ */
+
+public class Solution {
+
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        assert Checker.check(new Solution().runningSum(new int[]{1,2,3,4}), new int[]{1,3,6,10});
+        assert Checker.check(new Solution().runningSum(new int[]{1,1,1,1,1}), new int[]{1,2,3,4,5});
+        assert Checker.check(new Solution().runningSum(new int[]{3,1,2,10,1}), new int[]{3,4,6,16,17});
+    }
+
+}
---
+++ leetcode-java/src/problem/p1232checkifitisastraightline/Solution.java
@@ -0,0 +1,33 @@
+package problem.p1232checkifitisastraightline;
+
+/**
+ * 1232. Check If It Is a Straight Line
+ *
+ * https://leetcode-cn.com/problems/check-if-it-is-a-straight-line/
+ *
+ * You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point.
+ * Check if these points make a straight line in the XY plane.
+ */
+
+public class Solution {
+
+    public boolean checkStraightLine(int[][] coordinates) {
+        if (coordinates.length == 2) return true;
+        int l = coordinates.length, e = l - 1;
+        for (int i = 1; i < e; i++) {
+            if ((coordinates[i][0] - coordinates[0][0]) * (coordinates[i][1] - coordinates[e][1]) != (coordinates[i][1] - coordinates[0][1]) * (coordinates[i][0] - coordinates[e][0])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        assert new Solution().checkStraightLine(new int[][]{{0,0},{0,1},{0,-1}});
+
+        assert new Solution().checkStraightLine(new int[][]{{1,2},{2,3},{3,4},{4,5},{5,6},{6,7}});
+        assert !new Solution().checkStraightLine(new int[][]{{1,1},{2,2},{3,4},{4,5},{5,6},{7,7}});
+    }
+
+}
