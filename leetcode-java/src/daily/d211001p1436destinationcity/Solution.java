9b0bf32f614efff3731ee2093e6d8123a0e255d7
Jayson Wang <jayson@laboys.org>
2021-10-01 23:54:10 +0800

    daily

---
+++ leetcode-java/src/daily/d211001p1436destinationcity/Solution.java
@@ -0,0 +1,33 @@
+package daily.d211001p1436destinationcity;
+
+import java.util.*;
+
+/**
+ * 1436. Destination City
+ *
+ * https://leetcode-cn.com/problems/destination-city/
+ *
+ * You are given the array paths, where paths[i] = [cityAi, cityBi] means there
+ * exists a direct path going from cityAi to cityBi.
+ *
+ * Return the destination city, that is, the city without any path outgoing to another city.
+ *
+ * It is guaranteed that the graph of paths forms a line without any loop,
+ * therefore, there will be exactly one destination city.
+ */
+
+public class Solution {
+
+    public String destCity(List<List<String>> paths) {
+        Set<String> uniq = new HashSet<>();
+        for (var path : paths) uniq.add(path.get(0));
+        for (var path : paths) {
+            if (!uniq.contains(path.get(1))) return path.get(1);
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
