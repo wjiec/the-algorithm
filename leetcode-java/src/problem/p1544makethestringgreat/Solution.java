41a8af36b8499902864bcac135989480822ae179
Jayson <shadowman@shellboot.com>
2021-10-16 15:01:54 +0800

    daily

---
+++ leetcode-java/src/problem/p1544makethestringgreat/Solution.java
@@ -0,0 +1,40 @@
+package problem.p1544makethestringgreat;
+
+/**
+ * 1544. Make The String Great
+ *
+ * https://leetcode-cn.com/problems/make-the-string-great/
+ *
+ * Given a string s of lower and upper case English letters.
+ *
+ * A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+ *
+ * 0 <= i <= s.length - 2
+ * s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+ *
+ * To make the string good, you can choose two adjacent characters that make the string bad and remove them.
+ *
+ * You can keep doing this until the string becomes good.
+ *
+ * Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+ *
+ * Notice that an empty string is also good.
+ */
+
+public class Solution {
+
+    public String makeGood(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (var c : s.toCharArray()) {
+            if (sb.length() != 0 && Math.abs(sb.charAt(sb.length() - 1) - c) == 32) sb.deleteCharAt(sb.length() - 1);
+            else sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        assert new Solution().makeGood("leEeetcode").equals("leetcode");
+        assert new Solution().makeGood("abBAcC").equals("");
+    }
+
+}
---
+++ leetcode-java/src/problem/p1550threeconsecutiveodds/Solution.java
@@ -0,0 +1,30 @@
+package problem.p1550threeconsecutiveodds;
+
+/**
+ * 1550. Three Consecutive Odds
+ *
+ * https://leetcode-cn.com/problems/three-consecutive-odds/
+ *
+ * Given an integer array arr, return true if there are three consecutive odd numbers in the array.
+ *
+ * Otherwise, return false.
+ */
+
+public class Solution {
+
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int odd = 0;
+        for (var n : arr) {
+            if (n % 2 == 1) {
+                if (++odd >= 3) return true;
+            } else odd = 0;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        assert !new Solution().threeConsecutiveOdds(new int[]{2,6,4,1});
+        assert new Solution().threeConsecutiveOdds(new int[]{1,2,34,3,4,5,7,23,12});
+    }
+
+}
