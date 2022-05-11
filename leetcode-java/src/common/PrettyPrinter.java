8acc717dbbf0b22cddf5d9bfe1ebd767ea067831
Jayson <jayson@laboys.org>
2022-05-11 20:39:14 +0800

    TreePrinter

--- leetcode-java/src/common/PrettyPrinter.java
+++ leetcode-java/src/common/PrettyPrinter.java
@@ -1,9 +1,83 @@
 package common;
 
 import java.lang.reflect.Array;
-import java.util.Collection;
+import java.util.*;
 
 public class PrettyPrinter {
+
+    /**
+     * └   ┘
+     *                         <100>
+     *           ┌──────────────┴─────────────────┐
+     *           ----------------+------------------
+     *           |                                 |
+     *          200                               300
+     */
+
+    private static class TreePrinter {
+        private final TreeNode tree;
+        private boolean overflow = false;
+        private int height = 1, width = 0;
+        private final Map<TreeNode, Integer> offsets = new HashMap<>();
+        public TreePrinter(TreeNode tree) {
+            if ((this.tree = tree) != null) {
+                walkStruct(tree, height);
+                overflow = height > 10;
+                height = height % 11;
+                placeNode(tree, 0, (1 << height) - 1);
+            }
+        }
+
+        private void walkStruct(TreeNode node, int curr) {
+            if (curr > height) height = curr;
+            int width = String.valueOf(node.val).length();
+            if (width > this.width) this.width = width;
+
+            if (node.left != null) walkStruct(node.left, curr + 1);
+            if (node.right != null) walkStruct(node.right, curr + 1);
+        }
+
+        private void placeNode(TreeNode node, int l, int r) {
+            int mid = l + (r - l) / 2;
+            offsets.put(node, mid);
+
+            if (node.left != null) placeNode(node.left, l, mid);
+            if (node.right != null) placeNode(node.right, mid + 1, r);
+        }
+
+        @Override
+        public String toString() {
+            if (this.tree == null) return "── <null> ──";
+
+            StringBuilder sb = new StringBuilder();
+            Queue<TreeNode> queue = new ArrayDeque<>() {{ add(tree); }};
+            for (int i = 0; !queue.isEmpty() && i < 11; i++) {
+                StringBuilder curr = new StringBuilder();
+                if (i != 0) curr.append("\n");
+
+                for (int j = 0, n = queue.size(); j < n; j++) {
+                    TreeNode node = queue.remove();
+
+                    curr.append(middleString(node.val)).append(' ');
+
+                    if (node.left != null) queue.add(node.left);
+                    if (node.right != null) queue.add(node.right);
+                }
+                sb.append(curr);
+            }
+
+            if (this.overflow) sb.append("\n── <overflow> ──");
+            return sb.toString();
+        }
+
+        private String middleString(int val) {
+            int len = String.valueOf(val).length();
+            int padding = (width - len) / 2;
+            return String.format("<%s%d%s>", " ".repeat(padding), val, " ".repeat(width - len - padding));
+        }
+    }
+
+    public static void println(TreeNode tree) { System.out.println(new TreePrinter(tree)); }
 
     private static class Indent {
         private final int count;
@@ -48,6 +122,11 @@ public class PrettyPrinter {
             sb.append("]");
         }
         return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        println(TreeNode.build());
+        println(TreeNode.build(1,2,3,4,5,6,7,null,8,9,null,null,10,11,12));
     }
 
 }
