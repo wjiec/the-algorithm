fcfeb3236708579285bbdd3062d81afff3467a7f
jayson wang <jayson@laboys.org>
2023-07-24 13:23:34 +0800

    daily

--- leetcode-java/src/weekly/w354/A/Solution.java
+++ leetcode-java/src/weekly/w354/A/Solution.java
@@ -1,5 +1,17 @@
 package weekly.w354.A;
 
+/**
+ * 2778. Sum of Squares of Special Elements
+ *
+ * https://leetcode.cn/contest/weekly-contest-354/problems/sum-of-squares-of-special-elements/
+ *
+ * You are given a 1-indexed integer array nums of length n.
+ *
+ * An element nums[i] of nums is called special if i divides n, i.e. n % i == 0.
+ *
+ * Return the sum of the squares of all special elements of nums.
+ */
+
 public class Solution {
 
     public int sumOfSquares(int[] nums) {
--- leetcode-java/src/weekly/w354/B/Solution.java
+++ leetcode-java/src/weekly/w354/B/Solution.java
@@ -2,6 +2,27 @@ package weekly.w354.B;
 
 import java.util.Arrays;
 
+/**
+ * 2779. Maximum Beauty of an Array After Applying Operation
+ *
+ * https://leetcode.cn/contest/weekly-contest-354/problems/maximum-beauty-of-an-array-after-applying-operation/
+ *
+ * You are given a 0-indexed array nums and a non-negative integer k.
+ *
+ * In one operation, you can do the following:
+ *
+ * Choose an index i that hasn't been chosen before from the range [0, nums.length - 1].
+ * Replace nums[i] with any integer from the range [nums[i] - k, nums[i] + k].
+ * The beauty of the array is the length of the longest subsequence consisting of equal elements.
+ *
+ * Return the maximum possible beauty of the array nums after applying the operation any number of times.
+ *
+ * Note that you can apply the operation to each index only once.
+ *
+ * A subsequence of an array is a new array generated from the original array by deleting
+ * some elements (possibly none) without changing the order of the remaining elements.
+ */
+
 public class Solution {
 
     public int maximumBeauty(int[] nums, int k) {
