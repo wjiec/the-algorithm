2b67d9081f7f662e37b286961c5305ae34c549ba
Jayson Wang <jayson@laboys.org>
2023-12-19 23:07:22 +0800

    acwing 95

--- acwing/p95.cc
+++ acwing/p95.cc
@@ -3,81 +3,70 @@
 //
 
 #include <iostream>
-#include <vector>
+#include <cstring>
 
 using namespace std;
+const int state_count = 1 << 5;
+const int N = 5;
 
-constexpr static int MASK = ((1 << 5) - 1);
-constexpr static int INF = 1 << 20;
+typedef char Rect[10][10];
 
-int bitlen(int state) {
-    int ans = 0;
-    for (; state != 0; state >>= 1) {
-        if (state & 1) ans++;
+const int dirs[][2] = {{0, 0}, {0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+void turn(Rect rect, int x, int y) {
+    for (auto &dir : dirs) {
+        int dx = x + dir[0], dy = y + dir[1];
+        if (dx >= 0 && dx < N && dy >= 0 && dy < N) {
+            rect[dx][dy] ^= 1;
+        }
     }
-    return ans;
 }
 
-bool is_bit_set(int v, int i) {
-    return i >= 0 && ((v >> i) & 1);
-}
+int solution(char rect[10][10]) {
+    int ans = 1 << 20;
+    for (int s = 0; s < state_count; s++) {
+        Rect temp;
+        memcpy(temp, rect, 100);
 
-bool check(vector<int> &sw, int prev, int curr, int next) {
-    for (int i = 0; i < sw.size(); i++) {
-        int state = sw[i];
-        if (is_bit_set(prev, i)) state ^= 1;
-        if (is_bit_set(next, i)) state ^= 1;
-        if (is_bit_set(curr, i)) state ^= 1;
-        if (is_bit_set(curr, i - 1)) state ^= 1;
-        if (is_bit_set(curr, i + 1)) state ^= 1;
-        if (state == 0) return false;
-    }
-    return true;
-}
-
-int dfs(vector<vector<int>> &rects, int i, int pp, int p) {
-    if (i == rects.size()) {
-        return check(rects[i - 1], pp, p, 0) ? 0 : INF;
-    }
-
-    int ans = INF;
-    for (int curr = 0; curr <= MASK; curr++) {
-        // 当前如果选择了 curr 则可以判断前一行是否满足要求
-        if (i > 0 && !check(rects[i - 1], pp, p, curr)) {
-            continue;
+        int curr = 0;
+        for (int j = 0; j < N; j++) {
+            if (s & (1 << j)) {
+                curr++;
+                turn(temp, 0, j);
+            }
         }
 
-        ans = min(ans, dfs(rects, i + 1, p, curr) + bitlen(curr));
-    }
-    return ans;
-}
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N; j++) {
+                if (temp[i][j] == '0') {
+                    if (++curr > 6) break;
+                    turn(temp, i + 1, j);
+                }
+            }
+        }
 
-int solution(vector<vector<int>> &rect) {
-    return dfs(rect, 0, 0, 0);
+        bool ok = curr <= 6;
+        for (int j = 0; ok && j < N; j++) {
+            if (temp[N - 1][j] == '0') {
+                ok = false;
+                break;
+            }
+        }
+
+        if (ok) ans = min(ans, curr);
+    }
+
+    return ans > 6 ? -1 : ans;
 }
 
 int main() {
     int n = 0;
-    vector<vector<vector<int>>> rects;
-
     cin >> n;
+
     for (int i = 0; i < n; i++) {
-        vector<vector<int>> rect;
-        for (int j = 0; j < 5; j++) {
-            string s; cin >> s;
-
-            vector<int> line;
-            for (auto &c : s) {
-                line.push_back(c - '0');
-            }
-            rect.push_back(line);
-        }
-        rects.push_back(rect);
-    }
-
-    for (auto &rect : rects) {
-        int ans = solution(rect);
-        cout << (ans > 6 ? -1 : ans) << endl;
+        Rect rect;
+        for (int j = 0; j < N; j++) cin >> rect[j];
+        cout << solution(rect) << endl;
     }
 
     return 0;
