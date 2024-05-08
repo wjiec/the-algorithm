518c183b1e6d54059f0b5a1dcf1cfed06cd849d3
jayson <jayson@laboys.org>
2024-05-08 11:03:24 +0800

    daily

---
+++ leetcode-java/src/problem/p185departmenttopthreesalaries/solution.sql
@@ -0,0 +1,69 @@
+/*
+Table: Employee
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key (column with unique values) for this table.
+departmentId is a foreign key (reference column) of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of a department and its name.
+
+
+A company's executives are interested in seeing who earns the most money in each of the company's departments. A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
+
+Write a solution to find the employees who are high earners in each of the departments.
+
+Return the result table in any order.
+
+
+Schema:
+
+Create table If Not Exists Employee (id int, name varchar(255), salary int, departmentId int);
+Create table If Not Exists Department (id int, name varchar(255));
+Truncate table Employee;
+insert into Employee (id, name, salary, departmentId) values ('1', 'Joe', '85000', '1');
+insert into Employee (id, name, salary, departmentId) values ('2', 'Henry', '80000', '2');
+insert into Employee (id, name, salary, departmentId) values ('3', 'Sam', '60000', '2');
+insert into Employee (id, name, salary, departmentId) values ('4', 'Max', '90000', '1');
+insert into Employee (id, name, salary, departmentId) values ('5', 'Janet', '69000', '1');
+insert into Employee (id, name, salary, departmentId) values ('6', 'Randy', '85000', '1');
+insert into Employee (id, name, salary, departmentId) values ('7', 'Will', '70000', '1');
+Truncate table Department;
+insert into Department (id, name) values ('1', 'IT');
+insert into Department (id, name) values ('2', 'Sales');
+*/
+select
+    d.name as Department,
+    e.name as Employee,
+    e.salary as Salary
+from (
+    select
+        id,
+        name,
+        departmentId,
+        salary,
+        dense_rank() over (
+            partition by departmentId
+            order by salary desc
+        ) as ranking
+    from Employee
+) as e
+join Department as d on e.departmentId = d.id
+where e.ranking <= 3
