1aab343ffda8a8869fd7e73da576f204ea845304
jayson <jayson@laboys.org>
2024-10-11 16:59:47 +0800

    daily

--- leetcode-java/src/weekly/w417/C/Solution.java
+++ leetcode-java/src/weekly/w417/C/Solution.java
@@ -13,6 +13,11 @@ package weekly.w417.C;
  * at least once and exactly k consonants.
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static ability.AsciiString.isVowel;
+
 /** @noinspection DuplicatedCode*/
 public class Solution {
 
@@ -96,6 +101,44 @@ public class Solution {
         return a > 0 && e > 0 && i > 0 && o > 0 && u > 0 && other == k;
     }
 
+    private static class Optimization {
+        // 恰好 k 个辅音字母可以改成
+        //  - 至少 k 个辅音字母 - 至少 k + 1 个辅音字母
+        //      - >= k 的方案数是 a
+        //      - >= k + 1 的方案是 b
+        //      - 那么 = k 的方案数就是 a - b (因为 >= k 的方案包括 >= k + 1 的方案)
+        public long countOfSubstrings(String word, int k) {
+            char[] chars = word.toCharArray();
+
+            return least(chars, k) - least(chars, k + 1);
+        }
+
+        // 求 chars 中满足每个元音字母至少出现一次, 辅音字母至少出现 k 次的子字符串数量
+        private long least(char[] chars, int k) {
+            long ans = 0, consonants = 0;
+            Map<Character, Integer> vowels = new HashMap<>();
+            for (int l = 0, r = 0; r < chars.length; r++) {
+                if (isVowel(chars[r])) {
+                    vowels.merge(chars[r], 1, Integer::sum);
+                } else consonants++;
+
+                // 移动左边指针, 只要不满足条件
+                for (; vowels.size() == 5 && consonants >= k; l++) {
+                    if (isVowel(chars[l])) {
+                        if (vowels.merge(chars[l], -1, Integer::sum) == 0) {
+                            vowels.remove(chars[l]);
+                        }
+                    } else consonants--;
+                }
+
+                // 此时从 [0, l - 1] 都是满足条件的子字符串
+                ans += l;
+            }
+
+            return ans;
+        }
+    }
+
     public static void main(String[] args) {
         assert new Solution().countOfSubstrings("aoaiuefi", 1) == 4;
         assert new Solution().countOfSubstrings("aeouih", 0) == 1;
@@ -104,6 +147,13 @@ public class Solution {
         assert new Solution().countOfSubstrings("aeioqq", 1) == 0;
         assert new Solution().countOfSubstrings("aeiou", 0) == 1;
         assert new Solution().countOfSubstrings("ieaouqqieaouqq", 1) == 3;
+
+        assert new Optimization().countOfSubstrings("aoaiuefi", 1) == 4;
+        assert new Optimization().countOfSubstrings("aeouih", 0) == 1;
+        assert new Optimization().countOfSubstrings("iqeaouqi", 2) == 3;
+        assert new Optimization().countOfSubstrings("aeioqq", 1) == 0;
+        assert new Optimization().countOfSubstrings("aeiou", 0) == 1;
+        assert new Optimization().countOfSubstrings("ieaouqqieaouqq", 1) == 3;
     }
 
 }
