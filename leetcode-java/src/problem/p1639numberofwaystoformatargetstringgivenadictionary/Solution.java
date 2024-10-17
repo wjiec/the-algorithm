6c03071cf3b4051dc8da279392af6f0bad280ca5
jayson <jayson@laboys.org>
2024-10-17 18:28:27 +0800

    daily

--- leetcode-java/src/problem/p1639numberofwaystoformatargetstringgivenadictionary/Solution.java
+++ leetcode-java/src/problem/p1639numberofwaystoformatargetstringgivenadictionary/Solution.java
@@ -66,14 +66,80 @@ public class Solution {
         return memo[i][j + 1] = (ans % 1_000_000_007);
     }
 
+    // 超时, 很奇怪
+    private static class Optimization {
+        public int numWays(String[] words, String target) {
+            final int MOD = 1_000_000_007;
+            int wl = words[0].length(), tl = target.length();
+
+            // dp[i][j] 表示 使用 words[0:i) 的字符, 组装成 target[0:j) 的方案数
+            long[][] dp = new long[wl + 1][tl + 1]; dp[0][0] = 1;
+            // 枚举使用第 i 个字符进行转移
+            for (int i = 1; i <= wl; i++) {
+                int[] count = new int[128];
+                for (var word : words) count[word.charAt(i - 1)]++;
+
+                // 枚举可以转移到字符串 target 的哪一个位置
+                for (int j = 1; j <= i && j <= tl; j++) {
+                    char c = target.charAt(j - 1);
+                    if (count[c] == 0) continue;
+
+                    // 我们最少也需要 j - 1 个字符才能组装得到 target[0:j - 1]
+                    for (int k = i - 1; k >= j - 1; k--) {
+                        dp[i][j] = (dp[i][j] + count[c] * dp[k][j - 1]) % MOD;
+                    }
+                }
+            }
+
+            long ans = 0;
+            for (int i = tl; i <= wl; i++) {
+                ans = (ans + dp[i][tl]) % MOD;
+            }
+
+            return (int) ans;
+        }
+    }
+
+    // 状态压缩
+    private static class Optimization1 {
+        public int numWays(String[] words, String target) {
+            final int MOD = 1_000_000_007;
+            int wl = words[0].length(), tl = target.length();
+
+            long[] dp = new long[tl + 1]; dp[0] = 1;
+            // 枚举使用第 i 个字符进行转移
+            for (int i = 1; i <= wl; i++) {
+                int[] count = new int[128];
+                for (var word : words) count[word.charAt(i - 1)]++;
+
+                // 枚举可以转移到字符串 target 的哪一个位置
+                for (int j = Math.min(i, tl); j > 0; j--) {
+                    char c = target.charAt(j - 1);
+                    if (count[c] == 0) continue;
+
+                    dp[j] = (dp[j] + count[c] * dp[j - 1]) % MOD;
+                }
+            }
+
+            return (int) dp[tl];
+        }
+    }
+
     public static void main(String[] args) {
-        // a c c a
-        // b b b b
-        // c a c a
         assert new Solution().numWays(new String[]{"acca","bbbb","caca"}, "aba") == 6;
         assert new Solution().numWays(new String[]{"abba","baab"}, "bab") == 4;
         assert new Solution().numWays(new String[]{"abcd"}, "abcd") == 1;
         assert new Solution().numWays(new String[]{"abab","baba","abba","baab"}, "abba") == 16;
+
+        assert new Optimization().numWays(new String[]{"acca","bbbb","caca"}, "aba") == 6;
+        assert new Optimization().numWays(new String[]{"abba","baab"}, "bab") == 4;
+        assert new Optimization().numWays(new String[]{"abcd"}, "abcd") == 1;
+        assert new Optimization().numWays(new String[]{"abab","baba","abba","baab"}, "abba") == 16;
+
+        assert new Optimization1().numWays(new String[]{"acca","bbbb","caca"}, "aba") == 6;
+        assert new Optimization1().numWays(new String[]{"abba","baab"}, "bab") == 4;
+        assert new Optimization1().numWays(new String[]{"abcd"}, "abcd") == 1;
+        assert new Optimization1().numWays(new String[]{"abab","baba","abba","baab"}, "abba") == 16;
     }
 
 }
