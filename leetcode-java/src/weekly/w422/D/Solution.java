dca691bc5a1370386d4467ef74550a9ca943ab77
jayson <jayson@laboys.org>
2024-11-05 13:35:57 +0800

    daily

--- leetcode-java/src/weekly/w422/D/Solution.java
+++ leetcode-java/src/weekly/w422/D/Solution.java
@@ -4,6 +4,8 @@ import ability.Benchmark;
 
 import java.util.*;
 
+import static ability.Ability.Math.pow;
+
 /**
  * 3343. Count Number of Balanced Permutations
  *
@@ -18,6 +20,7 @@ import java.util.*;
  * Since the answer may be very large, return it modulo 109 + 7.
  *
  * A permutation is a rearrangement of all the characters of a string.
+ * @noinspection FieldCanBeLocal
  */
 
 public class Solution {
@@ -107,7 +110,81 @@ public class Solution {
         return ans;
     }
 
+    private static class Optimization {
+        private final int MOD = 1_000_000_007;
+        // 奇数位的字符数字之和等于偶数位的字符数字之和
+        //  - 字符的数量恰好是 n / 2 (下取整)
+        //  - 所取数字之和需要正好等于 sum / 2
+        // 先使用恰好型01背包计算可选的方案数, 再计算组合数
+        public int countBalancedPermutations(String num) {
+            int[] count = new int[10]; int sum = 0;
+            for (var c : num.toCharArray()) {
+                sum += c - '0';
+                count[c - '0']++;
+            }
+            if (sum % 2 != 0) return 0;
+
+            // 使用恰好 n / 2 个整数凑出答案为 sum / 2 的方案数
+            int n = num.length(), HALF_SUM = sum / 2, HALF_LEN = n / 2;
+            // dp[i][j] 表示使用 i 个数字组成和为 j 的方案数
+            int[][] dp = new int[HALF_LEN + 1][HALF_SUM + 1]; dp[0][0] = 1;
+            for (int i = 1; i <= n; i++) {
+                // 当前所使用的数字
+                int digit = num.charAt(i - 1) - '0';
+
+                // 枚举之前所使用的数字数量, 因为是从 i 递推到 i + 1, 所以得先算 i + 1
+                for (int j = Math.min(HALF_LEN, i) - 1; j >= 0; j--) {
+                    // 枚举使用 j 个数字加上当前数字之后的的所有可能的和
+                    for (int s = digit; s <= HALF_SUM; s++) {
+                        dp[j + 1][s] = (dp[j + 1][s] + dp[j][s - digit]) % MOD;
+                    }
+                }
+            }
+            long ans = dp[HALF_LEN][HALF_SUM];
+
+            // 对于选定的一个集合, 它的非重复排列方案有以下计算
+            //  - 首先不考虑重复问题, 排列方案数为 n! (n为集合大小)
+            //  - 例如对于数字 x, 有 x' x'' x''' 对于这三个 x 不管怎么分配实际上都是相同的
+            //      - 对于重复的数字 x, 也有 xn! 的排列方式
+            // 所以总的不重复方案数就是 n! / (c0! * c1! * ... * c9!)
+            //
+            // 左右两个集合的排列方案互不影响, 所以使用乘法
+            //  perm = (n1! * n2!) / (c0! * c1! * ... * c9!)
+            //
+            // perm 表示的是选定一种 a b 集合的方案然后排列的方案数
+            // 所以总的答案就是 np * perm (perm 与选择什么数字是没有关系的)
+            //
+            // 由于我们需要计算乘法, 所以这里需要用到费马小定理计算乘法逆元
+            final int MAX_N = n + 1;
+            long[] fac = new long[MAX_N + 1]; fac[1] = 1;
+            for (int i = 2; i < fac.length; i++) fac[i] = (fac[i - 1] * i) % MOD;
+            // 在 MOD 下的乘法逆元 inv_a = 1 / a = pow(a, MOD - 2, MOD)
+            //  - 对于阶乘 a * b * c 的逆元是 1 / (a * b * c) = pow(a * b * c, MOD - 2, MOD)
+            //      - 递推 a * b 的逆元是 1 / (a * b) = 1 / (a * b * c) * c
+            long[] inv = new long[MAX_N + 1]; inv[MAX_N] = pow(fac[MAX_N], MOD - 2, MOD);
+            for (int i = MAX_N; i > 0; i--) inv[i - 1] = (inv[i] * i) % MOD;
+
+            ans = (ans * ((fac[n / 2] * fac[n - n / 2]) % MOD)) % MOD;
+            for (var c : count) ans = (ans * inv[c]) % MOD;
+            return (int) ans;
+        }
+    }
+
+    /** @noinspection AssertWithSideEffects*/
     public static void main(String[] args) {
+        Benchmark.benchmark("Optimization", () -> {
+            assert new Optimization().countBalancedPermutations("987922212653624534086116331745003") == 721459843;
+            assert new Optimization().countBalancedPermutations("302203690210148694") == 215265033;
+            assert new Optimization().countBalancedPermutations("2156988039670517108") == 475443617;
+            assert new Optimization().countBalancedPermutations("6450063979135412487983626") == 444708717;
+            assert new Optimization().countBalancedPermutations("022") == 2;
+            assert new Optimization().countBalancedPermutations("2222") == 1;
+
+            assert new Optimization().countBalancedPermutations("123") == 2;
+            assert new Optimization().countBalancedPermutations("112") == 1;
+            assert new Optimization().countBalancedPermutations("12345") == 0;
+        });
+
         Benchmark.benchmark("permutation", () -> {
             new Solution().permutation(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
         });
