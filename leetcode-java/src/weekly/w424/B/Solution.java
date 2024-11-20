7cf09fee511f09161e445d63eaa37737ab24b9e2
jayson <jayson@laboys.org>
2024-11-20 19:00:05 +0800

    daily

--- leetcode-java/src/weekly/w424/B/Solution.java
+++ leetcode-java/src/weekly/w424/B/Solution.java
@@ -1,5 +1,24 @@
 package weekly.w424.B;
 
+/**
+ * 3355. Zero Array Transformation I
+ *
+ * https://leetcode.cn/contest/weekly-contest-424/problems/zero-array-transformation-i/
+ *
+ * You are given an integer array nums of length n and a 2D array queries, where queries[i] = [li, ri].
+ *
+ * For each queries[i]:
+ *
+ * Select a subset of indices within the range [li, ri] in nums.
+ * Decrement the values at the selected indices by 1.
+ * A Zero Array is an array where all elements are equal to 0.
+ *
+ * Return true if it is possible to transform nums into a Zero Array after processing
+ * all the queries sequentially, otherwise return false.
+ *
+ * A subset of an array is a selection of elements (possibly none) of the array.
+ */
+
 public class Solution {
 
     public boolean isZeroArray(int[] nums, int[][] queries) {
--- leetcode-java/src/weekly/w424/D/Solution.java
+++ leetcode-java/src/weekly/w424/D/Solution.java
@@ -1,24 +1,97 @@
 package weekly.w424.D;
 
+/**
+ * 3357. Minimize the Maximum Adjacent Element Difference
+ *
+ * https://leetcode.cn/contest/weekly-contest-424/problems/minimize-the-maximum-adjacent-element-difference/
+ *
+ * You are given an array of integers nums. Some values in nums are missing and are denoted by -1.
+ *
+ * You can choose a pair of positive integers (x, y) exactly once and replace each missing element with either x or y.
+ *
+ * You need to minimize the maximum absolute difference between adjacent elements of nums after replacements.
+ *
+ * Return the minimum possible difference.
+ */
+
 public class Solution {
 
     public int minDifference(int[] nums) {
-        int l = 0, r = Integer.MAX_VALUE, ans = 0;
+        int[] guards = new int[nums.length + 2];
+        System.arraycopy(nums, 0, guards, 1, nums.length);
+        for (int i = 1; i < guards.length - 1; i++) {
+            if (guards[i] != -1) {
+                guards[0] = guards[i];
+                break;
+            }
+        }
+        for (int i = guards.length - 2; i > 0; i--) {
+            if (guards[i] != -1) {
+                guards[guards.length - 1] = guards[i];
+                break;
+            }
+        }
+
+        int l = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] != -1 && nums[i] != -1) {
+                l = Math.max(l, Math.abs(nums[i - 1] - nums[i]));
+            }
+        }
+
+        int r = 1_000_000_007, ans = l;
         while (l < r) {
             int mid = l + (r - l) / 2;
-            if (check(nums, mid)) {
-                ans = mid; l = mid + 1;
-            } else r = mid;
+            if (check(guards, mid)) {
+                ans = mid; r = mid;
+            } else l = mid + 1;
         }
+
+        System.out.println(ans);
         return ans;
     }
 
     // 判断是否可以通过使用数对 (x, y) 填充 nums 中的所有 -1, 使得最大值为 max
     private boolean check(int[] nums, int max) {
-        return false;
+        // 首先判断所有共用 -1 的情况下是否满足
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == -1 && nums[i - 1] != -1 && nums[i + 1] != -1) {
+                int mid = (nums[i - 1] + nums[i + 1]) / 2;
+                if (Math.abs(mid - nums[i - 1]) > max) return false;
+                if (Math.abs(nums[i + 1] - mid) > max) return false;
+            }
+        }
+
+        // 求每个 -1 旁边非 -1 的数在 max 下的要求范围
+        // 如果所有的要求范围都可以使用两个子范围满足, 则说明该 max 是可以满足要求的
+        boolean first = true;
+        int l = Integer.MIN_VALUE, r = Integer.MAX_VALUE;
+        for (int i = 1, p = 0; i < nums.length - 1; i++) {
+            if (nums[i] != -1 && (nums[i - 1] == -1 || nums[i + 1] == -1)) {
+                l = Math.max(l, nums[i] - max);
+                r = Math.min(r, nums[i] + max);
+                if (l > r) {
+                    if (!first) return false;
+                    first = false;
+
+                    l = nums[i] - max;
+                    r = nums[i] + max;
+                    if (Math.abs(l - nums[p]) > max) return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
+        assert new Solution().minDifference(new int[]{14, -1, -1, 46}) == 11;
+        assert new Solution().minDifference(new int[]{2, -1, 4, -1, -1, 6}) == 1;
+
+        assert new Solution().minDifference(new int[]{1,12}) == 11;
+        assert new Solution().minDifference(new int[]{1,2,-1,10,8}) == 4;
+        assert new Solution().minDifference(new int[]{-1, -1, -1}) == 0;
+        assert new Solution().minDifference(new int[]{-1,10,-1,8}) == 1;
     }
 
 }
