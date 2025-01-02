f6d05fc162f9a2e06ad93085cb1434f49990b8f2
jayson <jayson@laboys.org>
2025-01-02 21:15:48 +0800

    daily

--- leetcode-java/src/weekly/w430/D/Solution.java
+++ leetcode-java/src/weekly/w430/D/Solution.java
@@ -1,5 +1,8 @@
 package weekly.w430.D;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static ability.Ability.Math.pow;
 
 public class Solution {
@@ -8,25 +11,33 @@ public class Solution {
 
     // 构造长度为 n 的数组, 数组的值为 [1, m], 同时数组有 k 个相邻元素数值相同的元素对
     public int countGoodArrays(int n, int m, int k) {
-        return (int) countGoodArrays(n, m, k, false);
+        return (int) ((m * count(n - 1, m, k)) % MOD);
     }
 
-    // preSelected 表示前面是否已经选择了一个值了
-    private long countGoodArrays(int n, long m, int k, boolean preSelected) {
-        // 如果要求有 n - 1 对相同的元素对, 那么就是所有元素都要一样的
-        if (k == n - 1) return m;
+    private final Map<Long, Long> memo = new HashMap<>();
+
+    // isFirst 表示是否当前是否是第一个元素
+    private long count(long n, long m, long k) {
+        if (k >= n) return k == n ? 1 : 0; // 无法创建一个长度为 n 但是要求有 >= n 个元素对
         // 如果 k == 0, 那么我们每个相邻的元素都不能相同
-        if (k == 0) return (int) ((m * pow(m - 1, n - 1, MOD)) % MOD);
+        if (k == 0) return pow(m - 1, n, MOD);
 
+        long key = (n << 32) | k;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        // 可以选择与前面不同的值, 但是这样就无法创建一个相同的元素对了
+        long ans = ((m - 1) * count(n - 1, m, k)) % MOD;
         // 选择与前面的值相同, 可以减少一个元素对, 那么当前值必须和前面的一样
-        long ans = countGoodArrays(n - 1, m, k - 1, false);
-        // 否则我们就需要选择与前面不同的值
-        ans += (m - 1) * countGoodArrays(n - 1, m, k, true);
+        ans = (ans + count(n - 1, m, k - 1)) % MOD;
 
-        return 1;
+        memo.put(key, ans);
+        return ans;
     }
 
     public static void main(String[] args) {
+        assert new Solution().countGoodArrays(4, 2, 2) == 6;
+        assert new Solution().countGoodArrays(3, 2, 1) == 4;
+        assert new Solution().countGoodArrays(5, 2, 0) == 2;
     }
 
 }
