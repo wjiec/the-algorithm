13a9ffeedacdeb1c0bed28eebe95b24a3c601d3c
jayson <jayson@laboys.org>
2025-01-22 22:20:27 +0800

    daily

--- leetcode-java/src/weekly/w432/B/Solution.java
+++ leetcode-java/src/weekly/w432/B/Solution.java
@@ -1,44 +1,93 @@
 package weekly.w432.B;
 
-import common.PrettyPrinter;
-
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class Solution {
 
     public int maximumAmount(int[][] coins) {
         int m = coins.length, n = coins[0].length;
-        int[][][] dp = new int[m + 1][n + 1][3];
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                int curr = coins[i - 1][j - 1];
+        // [i, j, c, s]
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{0, 0, 0, coins[0][0]});
+        queue.add(new int[]{0, 0, 1, Math.max(coins[0][0], 0)});
+        queue.add(new int[]{0, 0, 2, Math.max(coins[0][0], 0)});
 
-                // 0 表示没有使用感化技能
-                dp[i][j][0] = curr + Math.max(dp[i - 1][j][0], dp[i][j - 1][0]);
-                // 1 表示使用了 1 次感化技能
-                if (curr >= 0) dp[i][j][1] = curr + Math.max(dp[i - 1][j][1], dp[i][j - 1][1]);
-                if (curr < 0) dp[i][j][1] = Math.max(dp[i][j][1], Math.max(dp[i - 1][j][1], dp[i][j - 1][1])); // 感化
-                // 2 表示使用了 2 次感化技能
-                if (curr >= 0) dp[i][j][2] = curr + Math.max(dp[i - 1][j][2], dp[i][j - 1][2]);
-                if (curr < 0) dp[i][j][2] = Math.max(dp[i][j][2], Math.max(dp[i - 1][j][1], dp[i][j - 1][1])); // 感化
+        int ans = Integer.MIN_VALUE;
+        while (!queue.isEmpty()) {
+            var curr = queue.remove();
+            int i = curr[0], j = curr[1], c = curr[2], s = curr[3];
+            if (i == m - 1 && j == n - 1) ans = Math.max(ans, s);
 
-                System.out.printf("%d,%d = %d, dp = %s\n", i - 1, j - 1, coins[i - 1][j - 1], Arrays.toString(dp[i][j]));
+            if (i + 1 < m) {
+                queue.add(new int[]{i + 1, j, c, s + coins[i + 1][j]}); // op1
+                if (coins[i + 1][j] < 0 && c + 1 <= 2) queue.add(new int[]{i + 1, j, c + 1, s}); // op2
+            }
+
+            if (j + 1 < n) {
+                queue.add(new int[]{i, j + 1, c, s + coins[i][j + 1]}); // op1
+                if (coins[i][j + 1] < 0 && c + 1 <= 2) queue.add(new int[]{i, j + 1, c + 1, s}); // op2
             }
         }
 
-        PrettyPrinter.println(coins);
-        PrettyPrinter.println(dp);
+        return ans;
+    }
 
-        return Math.max(dp[m][n][0], Math.max(dp[m][n][1], dp[m][n][2]));
+    private static class DynamicProgramming {
+        public int maximumAmount(int[][] coins) {
+            int m = coins.length, n = coins[0].length;
+            // dp[i][j][k] 表示在 coins[i][j] 位置时使用 k 次感化能力的最大值
+            int[][][] dp = new int[m + 1][n + 1][3];
+            for (var r : dp) for (var c : r) c[0] = c[1] = c[2] = Integer.MIN_VALUE / 2;
+            dp[0][1][0] = dp[1][0][0] = 0;
+
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    int curr = coins[i - 1][j - 1];
+
+                    dp[i][j][0] = curr + Math.max(dp[i - 1][j][0], dp[i][j - 1][0]);
+
+                    dp[i][j][1] = curr + Math.max(dp[i - 1][j][1], dp[i][j - 1][1]);
+                    dp[i][j][1] = Math.max(dp[i][j][1], Math.max(dp[i - 1][j][0], dp[i][j - 1][0]));
+
+                    dp[i][j][2] = curr + Math.max(dp[i - 1][j][2], dp[i][j - 1][2]);
+                    dp[i][j][2] = Math.max(dp[i][j][2], Math.max(dp[i - 1][j][1], dp[i][j - 1][1]));
+                }
+            }
+
+            return Math.max(dp[m][n][0], Math.max(dp[m][n][1], dp[m][n][2]));
+        }
     }
 
     public static void main(String[] args) {
+        assert new DynamicProgramming().maximumAmount(new int[][]{
+            {-7, 12,  12, 13},
+            {-6, 19,  19, -6},
+            { 9, -2, -10, 16},
+            {-4, 14, -10, -9}
+        }) == 60;
+        assert new DynamicProgramming().maximumAmount(new int[][]{
+            {0,  1, -1},
+            {1, -2,  3},
+            {2, -3,  4}
+        }) == 8;
+        assert new DynamicProgramming().maximumAmount(new int[][]{
+            {10,10,10},
+            {10,10,10}
+        }) == 40;
+
+        assert new Solution().maximumAmount(new int[][]{
+            {-7, 12,  12, 13},
+            {-6, 19,  19, -6},
+            { 9, -2, -10, 16},
+            {-4, 14, -10, -9}
+        }) == 60;
         assert new Solution().maximumAmount(new int[][]{
             {0,  1, -1},
             {1, -2,  3},
             {2, -3,  4}
         }) == 8;
-        assert new Solution().maximumAmount(new int[][]{{10,10,10},{10,10,10},{10,10,10}}) == 40;
+        assert new Solution().maximumAmount(new int[][]{{10,10,10},{10,10,10}}) == 40;
     }
 
 }
--- leetcode-java/src/weekly/w432/C/Solution.java
+++ leetcode-java/src/weekly/w432/C/Solution.java
@@ -2,6 +2,9 @@ package weekly.w432.C;
 
 public class Solution {
 
+    public int minMaxWeight(int n, int[][] edges, int threshold) {
+    }
+
     public static void main(String[] args) {
     }
 
