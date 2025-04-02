1e8ed15b07705ae0b817f7da435fd06e7d9013a5
jayson <jayson@laboys.org>
2025-04-02 09:52:46 +0800

    daily

--- leetcode-java/src/weekly/w441/D/Solution.java
+++ leetcode-java/src/weekly/w441/D/Solution.java
@@ -2,6 +2,9 @@ package weekly.w441.D;
 
 import ability.Benchmark;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 3490. Count Beautiful Numbers
  *
@@ -26,36 +29,35 @@ public class Solution {
         int[] digits = new int[String.valueOf(v).length()];
         for (int i = digits.length - 1; i >= 0; i--, v /= 10) digits[i] = v % 10;
 
-        // 使用数位 dp 进行求解, 需要减去 0
-        return beautiful(digits, 0, 1, 0, true);
+        memo.clear();
+        // 使用数位 dp 进行求解
+        return beautiful(digits, 0, 1, 0, true, false);
     }
 
-    private int beautiful(int[] digits, int i, long mul, long sum, boolean limited) {
-        if (i == digits.length) return sum != 0 && mul % sum == 0 ? 1 : 0;
-        // 如果 mul == 0, 那么剩下的所有数都是合法的
-        //  - 这种情况只会发生在 x---0--- 的情况, 也就是不含前导 0 且中间出现了个 0
-        if (mul == 0) {
-            // 如果当前位受到限制, 那么最多只能有 int(digits[i:]) 个数
-            if (limited) {
-                int ans = 0;
-                for (; i < digits.length; i ++) ans = ans * 10 + digits[i];
-                return ans;
-            }
+    private final Map<Long, Integer> memo = new HashMap<>();
 
-            // 否则剩下的所有位都可以随便输入
-            return (int) Math.pow(10, digits.length - i);
+    private int beautiful(int[] digits, int i, long mul, long sum, boolean limited, boolean valid) {
+        if (i == digits.length) return valid && mul % sum == 0 ? 1 : 0;
+
+        long key = (mul << 32) | (sum << 16) | i;
+        if (!limited && memo.containsKey(key)) return memo.get(key);
+
+        int ans = 0;
+        if (!valid) ans += beautiful(digits, i + 1, mul, sum, false, false);
+
+        int upper = limited ? digits[i] : 9;
+        for (int j = valid ? 0 : 1; j <= upper; j++) {
+            ans += beautiful(digits, i + 1, mul * j, sum + j, limited && j == upper, true);
         }
 
-        int ans = 0, upper = limited ? digits[i] : 9;
-        for (int j = 0; j <= upper; j++) {
-            // 如果有前导 0 的话, 那么 mul 应该保持原样, 否则应该要乘进去的
-            ans += beautiful(digits, i + 1, (sum == 0 && j == 0) ? mul : (mul * j), sum + j, limited && j == upper);
-        }
+        memo.put(key, ans);
         return ans;
     }
 
     public static void main(String[] args) {
         Benchmark.benchmark("", () -> {
+            assert new Solution().beautifulNumbers(951, 955) == 2;
+
             assert new Solution().beautifulNumbers(1, 999999999) == 670349658;
             assert new Solution().beautifulNumbers(1, 509) == 167;
             assert new Solution().beautifulNumbers(571, 581) == 5;
