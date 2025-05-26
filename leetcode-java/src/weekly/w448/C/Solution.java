e74fc189e7234146ceaa54fa032c2fd5ad8329e2
jayson wang <jayson@laboys.org>
2025-05-26 23:06:58 +0800

    daily

--- leetcode-java/src/weekly/w448/C/Solution.java
+++ leetcode-java/src/weekly/w448/C/Solution.java
@@ -1,8 +1,5 @@
 package weekly.w448.C;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 3538. Merge Operations for Minimum Travel Time
  *
@@ -30,60 +27,39 @@ public class Solution {
 
     // 恰好合并 k 次, 使得总旅行时间最小
     public int minTravelTime(int l, int n, int k, int[] position, int[] time) {
-        // 每段的长度为 [a, b, c, d, e], 对应的时间为 [v, w, x, y, z]
-        //  - 不合并情况下的总时间为 T = av + bw + cx + dy + ez
-        // 如果合并 b 和 c, 总时间变为 av + (b+c) * w + d * (y + x) + ez
-        //  - 分解之后 = av + bw + cw + dy + dx + ez
-        //           = av + bw +    + dy + ez + cw + dx
-        //           = T + cw + dx - cx
-        //           = T + c * (w - x) + dx
-        //
-        // 此时每段的长度变为 [a, b+c, d, e], 对应的时间为 [v, w, x+y, z]
-        //  - 如果接着合并 b+c 和 d, 总时间变为 av + (b+c+d) * w + e * (x+y+z)
-        //      - 分解之后 = av + bw + cw + dw + ex + ey + ez
-        //               = av + bw +                    + ez + cw + dw + ex + ey
-        //               = T + cw + dw + ex + ey - cx - dy
-        //               = T + c * (w - x) + d * (w - y) + ex + ey
-        //
-        // 此时每段的长度变为 [a, b+c+d, e], 对应的时间为 [v, w, x+y+z]
-        //  - 如果接着合并 b+c+d 和 e, 总时间变为 av + (b+c+d+e) * w + 0 * (w + x + y + z)
-        //      - 分解之后 = av + bw + cw + dw + ew
-        //               = av + bw +                   + cw + dw + ew
-        //               = T + cw + dw + ew - cx - dy - ez
-        //               = T + c * (w - x) + d * (w - y) + e * (w - z)
-        //
-        // 分析可得, 如果合并长度为 (k1, k2 ..., kn) 和时间为 (t1, t2, ..., tn) 的段
-        //  总时间变为 = T + ()
+        int[] sum = new int[n];
+        for (int i = 0; i < n - 1; i++) sum[i + 1] = sum[i] + time[i];
 
-        // 先求出在不合并情况下的所有可能值
-        List<Segment> segments = new ArrayList<>();
-        for (int i = 0; i < n - 1; i++) {
-            segments.add(new Segment(position[i + 1] - position[i], time[i]));
+        memo = new int[n - 1][n - 1][k + 1];
+        return dfs(0, 0, k, position, sum);
+    }
+
+    private int[][][] memo = null;
+    private final static int INF = Integer.MAX_VALUE >> 1;
+
+    // 当前的数组是 [l, r], 剩下的 [r + 1, n) 数组还需要合并 k 次
+    public int dfs(int l, int r, int k, int[] pos, int[] sum) {
+        // 如果已经到达末尾了且还需要划分则不合法, 否则返回 0 表示是一个合理的划分
+        if (r == pos.length - 1) return k > 0 ? INF : 0;
+        if (memo[l][r][k] > 0) return memo[l][r][k];
+
+        // 将 [l, r] 合并之后, 会使得从 [l + 1, r) 的时间都叠加到 r 上
+        int ans = Integer.MAX_VALUE, st = sum[r + 1] - sum[l];
+        // 现在我们是把 [l, r] 范围的数据合并到下一个组里, 枚举下一个组的范围 [r + 1, next]
+        //  - [r + 1, next] 共有 next - (r + 1) + 1 个数, 构成了 next - (r + 1) + 1 - 1 个区间
+        //      = next - (r + 1) + 1 - 1 = next - r - 1 + 1 - 1 = next - r - 1
+        //  - 由于我们只可以合并 k 个, 所以 next - r - 1 <= k, 也就是 next < k + r + 1 + 1
+        for (int next = r + 1; next < Math.min(pos.length, k + r + 2); next++) {
+            // 合并 [r + 1, next] 个区间, 也就是使用将 [l + 1, r] 中的时间加到 pos[r] 上
+            //  - 得到的分数是 (pos[next] - pos[r]) * st
+            int curr = dfs(r + 1, next, k - (next - r - 1), pos, sum) + (pos[next] - pos[r]) * st;
+            ans = Math.min(ans, curr);
         }
 
-        System.out.println(dfs(segments, 0, 0, 0, k));
-        return dfs(segments, 0, 0, 0, k);
-    }
-
-    private record Segment (int len, int time) {
-        public int sum(int at) { return len * (at + time); }
-    }
-
-    private int dfs(List<Segment> segments, int i, int pt, int at, int k) {
-        if (i == segments.size()) return k == 0 ? 0 : Integer.MAX_VALUE / 2;
-        var curr = segments.get(i);
-
-        // 当前段选择不合并
-        int ans = curr.sum(at) + dfs(segments, i + 1, curr.time, 0, k);
-        // 也可以选择合并到前一段里
-        if (k > 0) ans = Math.min(ans, curr.time * pt + dfs(segments, i + 1, pt, at + curr.time, k - 1));
-
-        return ans;
+        return memo[l][r][k] = ans;
     }
 
     public static void main(String[] args) {
-        // 3 5, 5 8, 2 3
-        // 8 5,      2, 11
         assert new Solution().minTravelTime(10, 4, 1, new int[]{0,3,8,10}, new int[]{5,8,3,6}) == 62;
     }
 
