0e601fb82194392bf5b579f5584b8709d871a56b
jayson wang <jayson@laboys.org>
2025-06-11 23:57:46 +0800

    daily

--- leetcode-java/src/weekly/w450/D/Solution.java
+++ leetcode-java/src/weekly/w450/D/Solution.java
@@ -1,7 +1,8 @@
 package weekly.w450.D;
 
-import java.util.HashMap;
-import java.util.Map;
+import common.Tag;
+
+import java.util.*;
 
 /**
  * Q4. Minimum Weighted Subgraph With the Required Paths II
@@ -24,23 +25,111 @@ import java.util.Map;
 
 public class Solution {
 
-    private final Map<Integer, Map<Integer, Integer>> g = new HashMap<>();
+    // LCA 可以通过树上倍增算法实现
+    @SuppressWarnings("unchecked")
+    private static class LowestCommonAncestor {
+        private final int[] depth;
+        private final int[] distance;
+        private final int[][] table;
+
+        public LowestCommonAncestor(int[][] edges) {
+            int n = edges.length + 1;
+            int logN = 32 - Integer.numberOfLeadingZeros(n);
+
+            depth = new int[n];
+            distance = new int[n];
+            table = new int[n][logN];
+
+            List<int[]>[] g = new List[n];
+            Arrays.setAll(g, i -> new ArrayList<>());
+            for (var edge : edges) {
+                g[edge[0]].add(new int[]{edge[1], edge[2]});
+                g[edge[1]].add(new int[]{edge[0], edge[2]});
+            }
+            dfs(g, 0, -1);
+
+            // 开始倍增向上 2 ^ j 次可以到达的节点
+            for (int i = 0; i < n; i++) {
+                for (int j = 1; j < logN; j++) {
+                    // i 向上走 2 ^ (j - 1) 可以到达的地方是 p = table[i][j - 1]
+                    // 再向上走 2 ^ (j - 1) 可以到达的地方是 table[p][j - 1]
+                    int p = table[i][j - 1];
+                    // 如果已经走到跟节点了, 那就不能继续往上走了
+                    table[i][j] = p < 0 ? p : table[p][j - 1];
+                }
+            }
+        }
+
+        // 根据 LCA 计算 x 到 y 的距离
+        public int distance(int x, int y) {
+            return distance[x] + distance[y] - 2 * distance[query(x, y)];
+        }
+
+        // 找到 x 和 y 的最近公共祖先
+        public int query(int x, int y) {
+            // 我们需要保证 x 的深度小于 y 的深度, 然后让 y 向上到与 x 同一深度
+            if (depth[x] > depth[y]) { int t = x; x = y; y = t; }
+            y = kthAncestor(y, depth[y] - depth[x]);
+
+            // 如果 x == y, 说明 x 就是最近公共祖先
+            if (x == y) return x;
+
+            // 否则我们不断尝试走最大的步数尝试找到最近的公共祖先
+            for (int j = table[x].length - 1; j >= 0; j--) {
+                // 想上走 2 ^ j 次方之后的父节点是 px 和 py, 如果两者不同
+                // 说明最近公共祖先还在上面, 但是不够 2 ^ (j + 1)
+                int px = table[x][j], py = table[y][j];
+                if (px != py) { x = px; y = py; }
+            }
+            return table[x][0]; // 找到最近的公共祖先
+        }
+
+        // 找到节点 x 的第 k 个祖先
+        private int kthAncestor(int node, int k) {
+            for (; k != 0; k &= k - 1) {
+                node = table[node][Integer.numberOfTrailingZeros(k)];
+            }
+            return node;
+        }
+
+        // 计算每个节点的深度同时记录倍增中 j = 0 的节点
+        private void dfs(List<int[]>[] g, int curr, int parent) {
+            table[curr][0] = parent;
+            for (var next : g[curr]) {
+                if (next[0] != parent) {
+                    depth[next[0]] = depth[curr] + 1;
+                    distance[next[0]] = distance[curr] + next[1];
+                    dfs(g, next[0], curr);
+                }
+            }
+        }
+    }
 
     // 对每个查询 {src1, src2, dest} 需要找到一个最小的子树(任意节点和边组成的连通子集)权重和
     // 使得可以从 src1 和 src2 到达 dest
     //  - 节点数 3 <= n <= 1e5
     //  - 查询数 1 <= queries.length <= 1e5
+    @Tag({"LCA", "最近公共祖先", "包含给定节点的最小子树"})
     public int[] minimumWeight(int[][] edges, int[][] queries) {
-        for (var edge : edges) {
-            g.computeIfAbsent(edge[0], HashMap::new).merge(edge[1], edge[2], Integer::min);
-            g.computeIfAbsent(edge[1], HashMap::new).merge(edge[0], edge[2], Integer::min);
-        }
+        // 对于三个节点, 通过求 |A -> B| + |B -> C| + |C -> A| 的路径和, 此时恰好
+        // 每条路径走过两次, 将此路径和除以 2 就是答案.
+        //
+        // 求 |A -> B| 的路径和是通过求 |Root -> A| + |Root -> B| - 2 * |Root -> <LCA>| 的方式实现
+        //  - LCA 即 A 和 B 的最近公共祖先
+        //
+        // 扩展: 如果有 k 个点, 则为了保证每条路径经过两次, 需要使用 DFN 来保证遍历顺序
+        //  - 如果 Root -> A -> {B, C}, 在 B 子树中有 x 个点, C 子树中有 y 个点
+        //      - 按照 B 子树取一个, C 子树取一个的走法, |A -> B| 和 |A -> C| 会走至少 max(x, y) 次
+        //  - DFN: 也就是按照先序或者后序的方式遍历树时, 每个节点的遍历进入时间戳
+        //
+        // 三个节点的情况不需要应用 DFN, 因为三个节点是最小的两两关系的情况, 不管怎么走路径都是相同的
+        LowestCommonAncestor lca = new LowestCommonAncestor(edges);
 
-        // 从 src1 -> dest 加上从 src2 -> dest 一定是最优路线么?
-        //  - 有没有可能绕一圈 src1 -> src2 -> dest 反而代价更低
-        // 需要至多以 O(logN) 的时间复杂度处理每一个询问
         int[] ans = new int[queries.length];
-
+        for (int i = 0; i < queries.length; i++) {
+            int a = queries[i][0], b = queries[i][1], c = queries[i][2];
+            ans[i] = (lca.distance(a, b) + lca.distance(b, c) + lca.distance(c, a)) / 2;
+        }
         return ans;
     }
 
