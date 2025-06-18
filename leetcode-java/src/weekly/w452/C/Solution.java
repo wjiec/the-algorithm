5aac600610e6bcecc6376efed2a7c3c29f69641d
jayson wang <jayson@laboys.org>
2025-06-19 00:18:09 +0800

    daily

--- leetcode-java/src/weekly/w452/C/Solution.java
+++ leetcode-java/src/weekly/w452/C/Solution.java
@@ -32,78 +32,82 @@ public class Solution {
 
     private static final int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-    /** @noinspection DuplicatedCode */
     public int minMoves(String[] classroom, int energy) {
         int m = classroom.length, n = classroom[0].length();
         char[][] grid = new char[m][];
         for (int i = 0; i < m; i++) grid[i] = classroom[i].toCharArray();
 
         int sx = 0, sy = 0, cnt = 0;
-        int[][] id = new int[m][n];
+        List<Integer> id = new ArrayList<>();
         List<int[]> points = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 switch (grid[i][j]) {
-                    case 'L' -> { points.add(new int[]{i, j}); id[i][j] = cnt++; }
-                    case 'R' -> points.add(new int[]{i, j});
+                    case 'L' -> { points.add(new int[]{i, j}); id.add(cnt++); }
+                    case 'R' -> { points.add(new int[]{i, j}); id.add(-1); }
                     case 'S' -> { sx = i; sy = j; }
                 }
             }
         }
         if (cnt == 0) return 0; // 没有垃圾
 
-        points.add(new int[]{sx, sy});
-        int[][][][] distance = new int[m][n][m][n];
-        for (var point : points) {
-            int px = point[0], py = point[1];
-
-            int[][] dis = distance[px][py];
-            for (var row : dis) Arrays.fill(row, Integer.MAX_VALUE);
-
-            Queue<int[]> q = new ArrayDeque<>();
-            q.add(new int[]{px, py}); dis[px][py] = 0;
-            while (!q.isEmpty()) {
-                var curr = q.remove();
-                int x = curr[0], y = curr[1], nextStep = dis[x][y] + 1;
-                for (var dir : dirs) {
-                    int dx = x + dir[0], dy = y + dir[1];
-                    if (dx >= 0 && dx < m && dy >= 0 && dy < n && grid[dx][dy] != 'X' && dis[dx][dy] > nextStep) {
-                        dis[dx][dy] = nextStep; q.add(new int[]{dx, dy});
-                    }
+        int pl = points.size();
+        // [index, energy, mask, step]
+        Queue<int[]> queue = new ArrayDeque<>();
+        // 尝试从起点看看到每个垃圾点或者补给点的最短距离
+        int[] fromStart = distance(grid, sx, sy, points);
+        // 记忆化 seen[i][j][k] 表示当前在 i 点, 剩余能量为 j, 且当前已收集的垃圾为 k 是否已经遍历过
+        int[][][] seen = new int[pl][energy + 1][1 << cnt];
+        for (int i = 0; i < pl; i++) {
+            for (var row : seen[i]) Arrays.fill(row, Integer.MAX_VALUE);
+            // 如果可以从起点走到当前垃圾点或者补给点, 就将其加入队列
+            if (fromStart[i] <= energy) {
+                int pEnergy = energy - fromStart[i], pMask = 0;
+                switch (grid[points.get(i)[0]][points.get(i)[1]]) {
+                    case 'L' -> pMask |= 1 << id.get(i);
+                    case 'R' -> pEnergy = energy;
                 }
+
+                seen[i][pEnergy][pMask] = fromStart[i];
+                queue.add(new int[]{i, pEnergy, pMask, fromStart[i]});
             }
         }
-        points.remove(points.size() - 1);
 
-        int[][][][] seen = new int[m][n][energy + 1][1 << cnt];
-        for (var a : seen) for (var b : a) for (var c : b) Arrays.fill(c, 1000);
+        // dis[i][j] 表示从 point[i] 走到 point[j] 的最短距离
+        int[][] dis = new int[pl][];
+        for (int i = 0; i < pl; i++) {
+            dis[i] = distance(grid, points.get(i)[0], points.get(i)[1], points);
+        }
 
-        int ans = Integer.MAX_VALUE;
-        Queue<int[]> queue = new ArrayDeque<>();
-        seen[sx][sy][energy][(1 << cnt) - 1] = 0;
-        // [x, y, energy, mask, step]
-        queue.add(new int[]{sx, sy, energy, (1 << cnt) - 1, 0});
+        // 开始枚举所有可能的走法
+        int ans = Integer.MAX_VALUE, allMask = (1 << cnt) - 1;
         while (!queue.isEmpty()) {
             var curr = queue.remove();
-            int x = curr[0], y = curr[1], currEnergy = curr[2], currMask = curr[3], currStep = curr[4];
-            if (currMask == 0) { ans = Math.min(ans, currStep); continue; }
+            int i = curr[0], cEnergy = curr[1], cMask = curr[2], cStep = curr[3];
+            if ((cMask ^ allMask) == 0) { ans = Math.min(ans, cStep); continue; }
 
-            // 直接走到某一个垃圾点, 或者走到补给点
-            for (var point : points) {
-                int dx = point[0], dy = point[1], idMask = 1 << id[dx][dy];
-                boolean isResetPoint = grid[dx][dy] == 'R';
+            // 尝试从这个点走到其他的点, 最终完成遍历
+            for (int j = 0; j < pl; j++) {
+                int pEnergy = cEnergy - dis[i][j], pMask = cMask;
+                if (pEnergy < 0 || j == i) continue; // 无法走到或者相同的点
 
-                // 检查当前剩余的能量是否能走到这个目标点
-                int dis = distance[x][y][dx][dy];
-                if (dis <= currEnergy && (x != dx || y != dy)) { // 可以走到当前这个目标点
-                    int nextEnergy = currEnergy - dis, nextMask = currMask, nextStep = currStep + dis;
-                    if (isResetPoint) nextEnergy = energy;
-                    else if ((nextMask & idMask) != 0) nextMask ^= idMask;
-
-                    if (nextStep < seen[dx][dy][nextEnergy][nextMask]) {
-                        seen[dx][dy][nextEnergy][nextMask] = nextStep;
-                        queue.add(new int[]{dx, dy, nextEnergy, nextMask, nextStep});
+                // 检查是否需要更新下一步的 mask 以及 energy
+                switch (grid[points.get(j)[0]][points.get(j)[1]]) {
+                    case 'L' -> {
+                        // 已经收集过了就没必要再收集了
+                        if ((pMask & (1 << id.get(j))) != 0) {
+                            continue;
+                        }
+                        // 收集一个垃圾
+                        pMask |= 1 << id.get(j);
                     }
+                    case 'R' -> pEnergy = energy; // 恢复能量
+                }
+
+                int pStep = cStep + dis[i][j];
+                if (pStep < seen[j][pEnergy][pMask]) {
+                    seen[j][pEnergy][pMask] = pStep;
+                    queue.add(new int[]{j, pEnergy, pMask, pStep});
                 }
             }
         }
@@ -111,8 +115,54 @@ public class Solution {
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
+    // 计算从 (x, y) 到 points 中每个点的最短距离
+    private int[] distance(char[][] grid, int x, int y, List<int[]> points) {
+        int m = grid.length, n = grid[0].length;
+
+        int[][] seen = new int[m][n];
+        for (var row : seen) Arrays.fill(row, m * n + 1);
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{x, y}); seen[x][y] = 0;
+
+        while (!queue.isEmpty()) {
+            var curr = queue.remove();
+            int cx = curr[0], cy = curr[1], nextStep = seen[cx][cy] + 1;
+            for (var dir : dirs) {
+                int dx = cx + dir[0], dy = cy + dir[1];
+                if (dx >= 0 && dx < m && dy >= 0 && dy < n && grid[dx][dy] != 'X' && nextStep < seen[dx][dy]) {
+                    seen[dx][dy] = nextStep; queue.add(new int[]{dx, dy});
+                }
+            }
+        }
+
+        int[] ans = new int[points.size()];
+        for (int i = 0; i < points.size(); i++) {
+            ans[i] = seen[points.get(i)[0]][points.get(i)[1]];
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Benchmark.benchmark("", () -> {
+            assert new Solution().minMoves(new String[]{
+                "LLLRXLRSX.RXL",
+                "XRLL..XXR.R.R",
+                "RXR..R......R",
+                "X..XXRRRXXRR.",
+                "..RRRXRXR...R",
+                "XRRXRX.R.RRR.",
+                ".XR.XXR...R.R",
+                "X.RXRXXRX.XXR",
+                "XRX.RRXX.XR..",
+                "R.R.X.RRR...X",
+                "RRRXX...XXRR.",
+                "R.R.RXRXX..RR",
+                ".X......R..XR",
+                ".XRRRX...RX.R",
+                "R...X.X...XR."
+            }, 45) == 25;
+
             assert new Solution().minMoves(new String[]{
                 "S...................", // 0
                 "....................", // 1
