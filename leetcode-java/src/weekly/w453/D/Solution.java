352a675d98642c24325a8014129efc4d89705110
jayson wang <jayson@laboys.org>
2025-07-08 23:54:30 +0800

    daily

--- leetcode-java/src/weekly/w453/D/Solution.java
+++ leetcode-java/src/weekly/w453/D/Solution.java
@@ -1,7 +1,5 @@
 package weekly.w453.D;
 
-import java.util.Arrays;
-
 /**
  * Q4. Minimum Steps to Convert String with Operations
  *
@@ -27,49 +25,57 @@ import java.util.Arrays;
 
 public class Solution {
 
-    // 每个位置只能操作一次
+    // 每个位置只能操作一次, 也就是每种操作只能执行一次
     public int minOperations(String word1, String word2) {
-        return minOperations(word1.toCharArray(), word2.toCharArray(), 0, word1.length());
-    }
+        int n = word1.length();
+        char[] chars1 = word1.toCharArray();
+        char[] chars2 = word2.toCharArray();
 
-    private final int[] memo = new int[1 << 16];
-    { Arrays.fill(memo, -1); }
+        // dp[i] 表示在 [0, i) 内使得 word1 与 word2 一样的最小操作次数
+        int[] dp = new int[n + 1];
+        // 枚举右端点, 然后找到最小的左端点
+        for (int r = 0; r < n; r++) {
+            dp[r + 1] = Integer.MAX_VALUE; // 初始化
 
-    private int minOperations(char[] chars1, char[] chars2, int l, int r) {
-        int key = (l << 8) | r;
-        if (memo[key] != -1) return memo[key];
+            int forward = 0;
+            int[][] correct = new int[26][26];
+            for (int l = r; l >= 0; l--) {
+                int a = chars1[l] - 'a', b = chars2[l] - 'a';
+                if (a != b) {
+                    // 检查是否有相反的纠错存在
+                    //  - 如果有相反的纠错存在, 等于之前那次替换修改为交换
+                    if (correct[b][a] == 0) {
+                        correct[a][b]++; forward++;
+                    } else correct[b][a]--;
+                }
 
-        if (equals(chars1, chars2, l, r, false)) return memo[key] = 0;
-        if (equals(chars1, chars2, l, r, true)) return memo[key] = 1;
+                // 如果从 [l, r] 进行反转的话, 我们需要额外操作一次
+                int reversed = 1;
+                int[][] revCorrect = new int[26][26];
+                for (int p = l; p <= r; p++) {
+                    // 如果反转 [l, r] 范围内的字符, 对于在范围内的索引 p 我们将其表示为 l + i 的相对形式
+                    //  - 对于位置 p 的相对索引 i, 它反转之后相对位置在 rev = n - i - 1 处
+                    //      -> n = (r - l)
+                    //      -> p = l + i
+                    //          => i = p - l
+                    //      => rev = n - i - 1
+                    //             = (r - l) - (p - l) - 1
+                    //             = r - l - p + l - 1
+                    //             = r - p - 1
+                    //  - 也就是在 l + (r - p) - 1 位置
+                    a = chars1[l + (r - p)] - 'a'; b = chars2[p] - 'a';
+                    if (a != b) {
+                        if (revCorrect[b][a] == 0) {
+                            revCorrect[a][b]++; reversed++;
+                        } else revCorrect[b][a]--;
+                    }
+                }
 
-        // 联合使用操作 1 和 2, 找到 chars1 中所有需要修改的字符和目标字符
-        //  - 如果 chars1 中有一对 a -> b 和 b -> a 那么直接交换即可
-        //  - 否则需要使用操作 1 进行替换
-        int ans = 0;
-        int[][] correct = new int[26][26];
-        for (int i = l; i < r; i++) {
-            int a = chars1[i] - 'a', b = chars2[i] - 'a';
-            if (a != b) {
-                // 检查是否有相反的纠错存在
-                //  - 如果有相反的纠错存在, 等于之前那次替换修改为交换
-                if (correct[b][a] == 0) {
-                    correct[a][b]++; ans++;
-                } else correct[b][a]--;
+                dp[r + 1] = Math.min(dp[r + 1], dp[l] + Math.min(forward, reversed));
             }
         }
 
-        // 枚举子串
-        for (int i = l + 1; i < r; i++) {
-            ans = Math.min(ans, minOperations(chars1, chars2, l, i) + minOperations(chars1, chars2, i, r));
-        }
-        return memo[key] = ans;
-    }
-
-    private boolean equals(char[] chars1, char[] chars2, int l, int r, boolean rev) {
-        for (int i = 0, n = r - l; i < n; i++) {
-            if (chars1[rev ? (r - i - 1) : i] != chars2[i]) return false;
-        }
-        return true;
+        return dp[n];
     }
 
     public static void main(String[] args) {
