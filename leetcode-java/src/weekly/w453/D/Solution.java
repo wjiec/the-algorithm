1739f6ac8058a347991d5ca26dd0bdb7f79cff7d
jayson wang <jayson@laboys.org>
2025-07-09 23:30:15 +0800

    daily

--- leetcode-java/src/weekly/w453/D/Solution.java
+++ leetcode-java/src/weekly/w453/D/Solution.java
@@ -23,6 +23,7 @@ package weekly.w453.D;
  * Return the minimum number of operations required to transform word1 into word2.
  */
 
+@SuppressWarnings("DuplicatedCode")
 public class Solution {
 
     // 每个位置只能操作一次, 也就是每种操作只能执行一次
@@ -79,7 +80,68 @@ public class Solution {
         return dp[n];
     }
 
+    private static class Optimization {
+        public int minOperations(String word1, String word2) {
+            int n = word1.length();
+            char[] chars1 = word1.toCharArray();
+            char[] chars2 = word2.toCharArray();
+
+            // 预处理所有的反转字符串的最少操作系数, 对于 a ... b 和 b ... a 来说
+            //  - 如果一个字符串的前后字符反向相同, 则实际就等于中间 ... 的最小操作次数
+            //
+            // 预处理方式是使用中心扩展法, 当 i 是偶数时, 枚举的是奇数长度; 当 i 是奇数时, 枚举的是偶数长度
+            int[][] rev = new int[n][n];
+            for (int i = 0; i < 2 * n - 1; i++) {
+                int[][] correct = new int[26][26];
+                int l = i / 2, r = (i + 1) / 2, curr = 1; // 默认需要一次反转
+                while (l >= 0 && r < n) {
+                    // 左半边
+                    int a = chars1[l] - 'a', b = chars2[r] - 'a';
+                    if (a != b) {
+                        if (correct[a][b] == 0) {
+                            correct[b][a]++; curr++;
+                        } else correct[a][b]--;
+                    }
+
+                    // 右半边(如果有的话)
+                    a = chars1[r] - 'a'; b = chars2[l] - 'a';
+                    if (l != r && a != b) {
+                        if (correct[a][b] == 0) {
+                            correct[b][a]++; curr++;
+                        } else correct[a][b]--;
+                    }
+
+                    rev[l--][r++] = curr;
+                }
+            }
+
+            // 剩下的操作与原始版本一致, 最近降低至 O(n^2)
+            int[] dp = new int[n + 1];
+            for (int r = 0; r < n; r++) {
+                dp[r + 1] = Integer.MAX_VALUE;
+
+                int curr = 0;
+                int[][] correct = new int[26][26];
+                for (int l = r; l >= 0; l--) {
+                    int a = chars1[l] - 'a', b = chars2[l] - 'a';
+                    if (a != b) {
+                        if (correct[a][b] == 0) {
+                            correct[b][a]++; curr++;
+                        } else correct[a][b]--;
+                    }
+
+                    dp[r + 1] = Math.min(dp[r + 1], dp[l] + Math.min(curr, rev[l][r]));
+                }
+            }
+            return dp[n];
+        }
+    }
+
     public static void main(String[] args) {
+        assert new Optimization().minOperations("abcdf", "dacbe") == 4;
+        assert new Optimization().minOperations("abceded", "baecfef") == 4;
+        assert new Optimization().minOperations("abcdef", "fedabc") == 2;
+
         assert new Solution().minOperations("abcdf", "dacbe") == 4;
         assert new Solution().minOperations("abceded", "baecfef") == 4;
         assert new Solution().minOperations("abcdef", "fedabc") == 2;
