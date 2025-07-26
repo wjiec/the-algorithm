79753069c0d326b90f4f5153786fa302be55cbd6
jayson wang <jayson@laboys.org>
2025-07-26 23:04:53 +0800

    daily

--- leetcode-java/src/weekly/bw159/D/Solution.java
+++ leetcode-java/src/weekly/bw159/D/Solution.java
@@ -1,8 +1,11 @@
 package weekly.bw159.D;
 
 import common.Checker;
+import common.Tag;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Q4. Kth Smallest Path XOR Sum
@@ -32,6 +35,7 @@ import java.util.*;
 public class Solution {
 
     // 对于每个查询 [u, k], 找到 u 的子树中第 k 小的不同异或和
+    @Tag({"莫队", "树状数组第 k 小", "集合中的第 k 小"})
     public int[] kthSmallest(int[] par, int[] vals, int[][] queries) {
         int n = par.length;
         // 先要求出每个节点的异或和是多少
@@ -39,41 +43,109 @@ public class Solution {
         //      - l, r 可以通过遍历时间戳得到
         //
         // 问题变成: 在一个数组中求区间子数组的第 k 小数
-        //  - 倍增来做?
+        //  - 莫队 + 树状数组第 k 小
+        //
+        // 莫队 (Mo's Algorithm) 是一种静态查询处理算法，通常用于离线处理区间查询
+        //  - 核心思想是将所有查询离线保存，然后按特定顺序排序处理每个区间，最大程度地减少每步修改操作的开销，从而提高整体效率
+        //  - 它利用了 "区间可以逐步扩展和缩小，每次只变动一小部分元素" 的特点
 
         List<Integer>[] g = new List[n];
         Arrays.setAll(g, i -> new ArrayList<>());
         for (int i = 1; i < n; i++) g[par[i]].add(i);
 
         // 遍历树上的节点并计算异或和, 时间戳
-        int[] enter = new int[n], leave = new int[n];
-        dfs(g, 0, vals, vals[0], enter, leave);
+        int[] xors = new int[n], enter = new int[n], leave = new int[n];
+        dfs(g, 0, vals, vals[0], xors, enter, leave);
 
-        int[] ans = new int[queries.length];
-        Map<Integer, List<Integer>> memo = new HashMap<>();
-        for (int i = 0; i < queries.length; i++) {
-            int node = queries[i][0], k = queries[i][1] - 1;
-            if (!memo.containsKey(node)) {
-                int l = enter[node], r = leave[node];
-                List<Integer> curr = new ArrayList<>(new HashSet<>(pre.subList(l, r)));
-                curr.sort(Integer::compare);
-                memo.put(node, curr);
-            }
+        // 对所有的异或和进行离散化
+        int[] sorted = Arrays.copyOf(xors, n);
+        Arrays.sort(sorted);
+        for (int i = 0; i < n; i++) {
+            xors[i] = Arrays.binarySearch(sorted, xors[i]) + 1;
+        }
 
-            var curr = memo.get(node);
-            ans[i] = k >= curr.size() ? -1 : curr.get(k);
+        // 将原数据分为 sqrt(n) 块
+        int ql = queries.length;
+        int blockSize = (int) Math.ceil(n / Math.sqrt(ql * 2));
+
+        record Query(int block, int l, int r, int k, int i) {}
+        Query[] qs = new Query[ql];
+        for (int i = 0; i < ql; i++) {
+            int in = enter[queries[i][0]], out = leave[queries[i][0]];
+            qs[i] = new Query(in / blockSize, in, out, queries[i][1], i);
+        }
+
+        // 对于区间 [l,r], 以 l 所在块的编号为第一关键字, r 为第二关键字从小到大排序
+        Arrays.sort(qs, (a, b) -> {
+            if (a.block != b.block) return a.block - b.block;
+            // 奇偶化排序即对于属于奇数块的询问，r 按从小到大排序，对于属于偶数块的排序，r 从大到小排序
+            // 这样我们的 r 指针在处理完这个奇数块的问题后，将在返回的途中处理偶数块的问题
+            // 再向 n 移动处理下一个奇数块的问题，优化了 r 指针的移动次数
+            //
+            // 一般情况下，这种优化能让程序快 30% 左右
+            return a.block % 2 == 0 ? (a.r - b.r) : (b.r - a.r);
+        });
+
+        int[] cnt = new int[n + 1];
+        FenwickTree t = new FenwickTree(n + 1);
+
+        int[] ans = new int[ql];
+        for (int l = 0, r = 0, i = 0; i < ql; i++) {
+            var query = qs[i];
+            while (l < query.l) move(xors[l++], -1, cnt, t);
+            while (l > query.l) move(xors[--l], 1, cnt, t);
+            while (r < query.r) move(xors[r++], 1, cnt, t);
+            while (r > query.r) move(xors[--r], -1, cnt, t);
+
+            int curr = t.kth(query.k) - 1;
+            ans[query.i] = curr < n ? sorted[curr] : -1;
         }
 
         return ans;
     }
 
-    private int clock = 0;
-    private final List<Integer> pre = new ArrayList<>();
+    private void move(int v, int delta, int[] cnt, FenwickTree t) {
+        if (delta > 0) {
+            if (cnt[v]++ == 0) t.update(v, 1);
+        } else {
+            if (--cnt[v] == 0) t.update(v, -1);
+        }
+    }
 
-    private void dfs(List<Integer>[] g, int curr, int[] vals, int xor, int[] enter, int[] leave) {
-        pre.add(xor); enter[curr] = clock++;
+    private static class FenwickTree {
+        private final int[] tree;
+        private final int highestBit;
+        public FenwickTree(int n) {
+            tree = new int[n + 1];
+            highestBit = Integer.highestOneBit(n);
+        }
+
+        public void update(int i, int v) {
+            for (; i < tree.length; i += i & -i) {
+                tree[i] += v;
+            }
+        }
+
+        // 求第 k 小 (k 从 1 开始)
+        public int kth(int k) {
+            int ans = 0;
+            for (int b = highestBit; b > 0; b >>= 1) {
+                int next = ans | b;
+                if (next < tree.length && k > tree[next]) {
+                    k -= tree[next];
+                    ans = next;
+                }
+            }
+            return ans + 1;
+        }
+    }
+
+    private int clock = 0;
+
+    private void dfs(List<Integer>[] g, int curr, int[] vals, int xor, int[] xors, int[] enter, int[] leave) {
+        xors[clock] = xor; enter[curr] = clock++;
         for (var next : g[curr]) {
-            dfs(g, next, vals, xor ^ vals[next], enter, leave);
+            dfs(g, next, vals, xor ^ vals[next], xors, enter, leave);
         }
         leave[curr] = clock;
     }
