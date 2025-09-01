f5e55be2ed07db2b39bc910ddacdb734d0102e0b
jayson <jayson@laboys.org>
2025-09-01 13:32:10 +0800

    daily

--- leetcode-java/src/weekly/w460/D/Solution.java
+++ leetcode-java/src/weekly/w460/D/Solution.java
@@ -1,5 +1,7 @@
 package weekly.w460.D;
 
+import ability.Benchmark;
+
 /**
  * Q4. Partition Array for Maximum XOR and AND
  *
@@ -24,47 +26,72 @@ package weekly.w460.D;
 public class Solution {
 
     public long maximizeXorAndXor(int[] nums) {
-        int n = nums.length, MAX_N = 1 << n, MASK = MAX_N - 1;
+        int n = nums.length, MAX_N = 1 << n, max = 0;
+        for (var v : nums) max = Math.max(max, v);
 
-        // xorSum[i] = {g1, g2, all} 表示 i 的位图中所有数的组合
-        //  - (g1, g2) 表示分成两组的异或值, all 表示将所有数都进行 xor 的值
-        long ans = 0;
-        long[][] xorSum = new long[MAX_N][3];
+        int[] a = new int[MAX_N], x = new int[MAX_N]; a[0] = -1;
         for (int i = 0; i < n; i++) {
-            int bitset = 1 << i, v = nums[i];
-            for (int mask = 0; mask < bitset; mask++) {
-                // 自己作为一组或者加入之前的组
-                xorSum[bitset | mask][2] = Math.max(xorSum[mask][2] ^ v, v);
-                // 选择加入 g1 或者 g2
-                if ((xorSum[mask][0] ^ v) + xorSum[mask][1] > xorSum[mask][0] + (xorSum[mask][1] ^ v)) {
-                    xorSum[bitset | mask][0] = xorSum[mask][0] ^ v;
-                    xorSum[bitset | mask][1] = xorSum[mask][1];
-                } else {
-                    xorSum[bitset | mask][0] = xorSum[mask][0];
-                    xorSum[bitset | mask][1] = xorSum[mask][1] ^ v;
-                }
-
-                ans = Math.max(ans, Math.max(xorSum[bitset | mask][2], Math.max(xorSum[bitset | mask][0], xorSum[bitset | mask][1])));
+            int v = nums[i], bit = 1 << i;
+            for (int j = 0; j < bit; j++) {
+                a[bit | j] = a[j] & v;
+                x[bit | j] = x[j] ^ v;
             }
         }
+        a[0] = 0; // 恢复原始状态
 
-        // 枚举所有组合的情况
-        long[] and = new long[MAX_N];
-        for (int i = 0; i < n; i++) {
-            int bitset = 1 << i, v = nums[i];
-            for (int mask = 0; mask < bitset; mask++) {
-                and[bitset | mask] = and[mask] & v;
-
-                int xor = MASK ^ (bitset | mask);
-                ans = Math.max(ans, and[bitset | mask] + Math.max(xorSum[xor][0] + xorSum[xor][1], xorSum[xor][2]));
-            }
+        long ans = 0; int sz = 32 - Integer.numberOfLeadingZeros(max);
+        for (int b = 0; b < MAX_N; b++) {
+            int s = (MAX_N - 1) ^ b;
+            ans = Math.max(ans, a[b] + maxXor(s, x[s], nums, sz));
         }
-
-        System.out.println(ans);
         return ans;
     }
 
+    private long maxXor(int mask, int xor, int[] nums, int sz) {
+        XorBasis b = new XorBasis(sz);
+        for (int i = 0; i < nums.length; i++) {
+            if ((mask & (1 << i)) != 0) {
+                b.insert(nums[i] & ~xor);
+            }
+        }
+        return xor + b.maxXor() * 2L;
+    }
+
+    private static class XorBasis {
+        private final int[] base;
+        public XorBasis(int n) { base = new int[n]; }
+        public void insert(int x) {
+            while (x > 0) {
+                int i = 31 - Integer.numberOfLeadingZeros(x); // x 的最高位
+                if (base[i] == 0) { // x 和之前的基是线性无关的
+                    base[i] = x; // 新增一个基, 最高位是 i
+                    return;
+                }
+                // 保证参与 maxXor 的基的最高位是互不相同的, 方便我们做贪心
+                x ^= base[i];
+            }
+        }
+        public int maxXor() {
+            int ans = 0;
+            // 从高到低贪心, 越高的位越应该是 1
+            // 由于每个位的基至多一个, 所以每个位只需要考虑异或一个基, 若能变大就应用
+            for (int i = base.length - 1; i >= 0; i--) {
+                ans = Math.max(ans, ans ^ base[i]);
+            }
+            return ans;
+        }
+    }
+
     public static void main(String[] args) {
+        Benchmark.benchmark("", () -> {
+            assert new Solution().maximizeXorAndXor(new int[]{
+                20194931,414512694,363723219,512267527,16746413,414429915,752676171,
+                296435179,611918530,159845273,329829245,415075965,61816591,279334329,
+                284479182,260505719,297930436,274359599,391282927
+            }) == 2480998406L;
+        });
+        assert new Solution().maximizeXorAndXor(new int[]{67,235,126}) == 428;
+
         assert new Solution().maximizeXorAndXor(new int[]{2, 3}) == 5;
         assert new Solution().maximizeXorAndXor(new int[]{1, 3, 2}) == 6;
         assert new Solution().maximizeXorAndXor(new int[]{2, 3, 6, 7}) == 15;
