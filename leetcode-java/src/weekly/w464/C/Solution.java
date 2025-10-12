b66a001ec90d743f9364c8ce95a8ee608d47ef50
jayson wang <jayson@laboys.org>
2025-10-12 23:44:48 +0800

    daily

--- leetcode-java/src/weekly/w464/C/Solution.java
+++ leetcode-java/src/weekly/w464/C/Solution.java
@@ -27,9 +27,26 @@ public class Solution {
         //  - 在 i < j && nums[i] > nums[j] 时, 往后跳, 大 -> 小
         //  - 在 j < i && nums[j] > nums[i] 时, 往前跳, 小 -> 大
         //
-        // 反向思考:
-        //  - 大的数的答案可以传播给后面较小的数的位置
-        //  - 小的数的答案可以传播到前面较大的数的位置
+        // 由于我们可以往前跳到更大的数, 往后只能跳到更小的数字上, 所以有:
+        //  - 如果存在 nums[i] = x, nums[j] = y, 且 x > y
+        //  - 那么 [i, j] 范围内的元素可以任意跳转
+        //      - 在 (i, j) 之间的数如果 > y, 则可以直接从 j 跳转过去
+        //      - 在 (i, j) 之间的数如果 <= y, 则可以先跳到 i, 再从 i 跳转过去
+        //
+        // 如果存在在范围 R2 = [l2, r2] 中的所有数均大于紧前范围 R1 = [l1, r2), 则无法从 R1 跳转到 R2
+        //  - 找到左边最大的数小于右边最小的数, 则当前位置就是一个分割线(无法从分割线左边跳到分割线右边)
+        int[] preMax = new int[nums.length]; preMax[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) preMax[i] = Math.max(preMax[i - 1], nums[i]);
+
+        int sufMin = Integer.MAX_VALUE, ans = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            // 找到了一个分割线, 我们无法从位置 i 跳到 [i + 1, n) 的位置上, 所以答案就是 [?, i] 的最大值
+            if (preMax[i] <= sufMin) ans = preMax[i];
+
+            // 记录后缀最小值并保存答案
+            sufMin = Math.min(sufMin, nums[i]); nums[i] = ans;
+        }
+
         return nums;
     }
 
