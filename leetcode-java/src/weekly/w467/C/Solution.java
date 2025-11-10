c6dfd526d2c7a7b4b519f1b15906ca39e0f7328b
jayson <jayson@laboys.org>
2025-11-10 14:15:32 +0800

    daily

--- leetcode-java/src/weekly/w467/C/Solution.java
+++ leetcode-java/src/weekly/w467/C/Solution.java
@@ -2,26 +2,54 @@ package weekly.w467.C;
 
 import java.util.Arrays;
 
+/**
+ * Q3. Subsequence Sum After Capping Elements
+ *
+ * https://leetcode.cn/contest/weekly-contest-467/problems/subsequence-sum-after-capping-elements/
+ *
+ * You are given an integer array nums of size n and a positive integer k.
+ *
+ * An array capped by value x is obtained by replacing every element nums[i] with min(nums[i], x).
+ *
+ * For each integer x from 1 to n, determine whether it is possible to choose a subsequence
+ * from the array capped by x such that the sum of the chosen elements is exactly k.
+ *
+ * Return a 0-indexed boolean array answer of size n, where answer[i] is true
+ * if it is possible when using x = i + 1, and false otherwise.
+ */
+
 public class Solution {
 
     public boolean[] subsequenceSumAfterCapping(int[] nums, int k) {
+        int n = nums.length;
         // ans[i] 表示是否可以通过在经过处理后找到一个子序列的和为 k
         //  - 处理操作: 将数组中的所有位置 j 修改为 min(nums[j], i)
+        boolean[] ans = new boolean[n];
 
         // 由于我们只需要选择子序列, 所以我们可以对数组进行排序
         Arrays.sort(nums);
 
-        // 枚举我们需要截断的数字 x, 在数组中找到 >= x 的第一个位置 i
-        //  - 此时在 [0, i) 都是小于 x 的,
-        //  - 在 [i, n) 都是大于等于 x 的, 经过操作之后, 也就是此时有 n - i 个 x 在数组中
-        //
-        // 此时的总和为 sum([0, i)) + (n - i) * x
-        //  - 如果不足, 则直接返回 false
-        //
-        // 如果需要凑数的话, 需要 k / x 个 x, 加上一个 k % x
-        //  - 也有可能是 k / x - 1 个 x, 加上 a, b 两个数, 满足 (a + b) % x = k % x
-        //  - 如果 x 的数量不够的话, 也还是需要在 [0, i) 内凑齐
-        return new boolean[0];
+        // dp[i] 表示是否能组合出和为 i 的子序列, 不选元素时和为 0
+        boolean[] dp = new boolean[k + 1]; dp[0] = true;
+        // 枚举当前的最大值 x 以及当前在数组中 x 出现的位置
+        for (int i = 0, x = 1; x <= n; x++) {
+            // 考虑所有恰好等于 x 的元素, 所有小于 x 的数之前已经考虑过了
+            for (; i < n && nums[i] == x; i++) {
+                for (int j = k; j >= nums[i]; j--) {
+                    dp[j] = dp[j] || dp[j - x];
+                }
+            }
+
+            // 枚举选择了 j 个 x
+            for (int j = 0; j <= Math.min(n - i, k / x); j++) {
+                if (dp[k - j * x]) {
+                    ans[x - 1] = true;
+                    break;
+                }
+            }
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
--- leetcode-java/src/weekly/w467/D/Solution.java
+++ leetcode-java/src/weekly/w467/D/Solution.java
@@ -1,5 +1,7 @@
 package weekly.w467.D;
 
+import common.PrettyPrinter;
+
 /**
  * Q4. Number of Stable Subsequences
  *
@@ -18,10 +20,39 @@ package weekly.w467.D;
 public class Solution {
 
     public int countStableSubsequences(int[] nums) {
-        return 1;
+        final int n = nums.length, MOD = 1_000_000_007;
+        // 考虑最后一个元素 x 选或者不选
+        //  - 不选: 变为在 [0, n - 2] 中有多少个稳定子序列
+        //  - 选:
+        //      - x 单独作为一个子序列
+        //      - x 是偶数
+        //          - 可以添加到末尾为奇数的子序列里, 也就是从 [0, n - 2] 里有多少个末尾为奇数的子序列
+        //          - 可以添加到末尾为奇数、偶数的子序列里(不能出现连续 3 个偶数), 也就是从 [0, n - 2] 里有多少个末尾为奇数, 偶数的子序列
+        //      - x 是奇数
+        //          - 可以添加到末尾为偶数的子序列里, 也就是从 [0, n - 2] 里有多少个末尾为偶数的子序列
+        //          - 可以添加到末尾为偶数、奇数的子序列里(不能出现连续 3 个奇数), 也就是从 [0, n - 2] 里有多少个末尾为偶数, 奇数的子序列
+
+        // dp[i][j][k] 表示在[0, i), 末尾元素的奇偶性为 j, 且末尾恰好有连续 k 个奇偶性都为 j 的元素
+        //  - 也就是第 k + 1 个元素的奇偶性与 x 相反
+        long[][][] dp = new long[n + 1][2][3];
+        for (int i = 1; i <= n; i++) {
+            // 设当前元素 nums[i - 1] 的奇偶性为 x
+            int x = nums[i - 1] & 1;
+            // 当 j == 1 时, 表示只能从末尾为 x ^ 1 的转移过来; 或者是自己单独作为一个字序列
+            dp[i][x][1] = (dp[i - 1][x][1] + dp[i - 1][x ^ 1][1] + dp[i - 1][x ^ 1][2] + 1) % MOD;
+            // 当 j == 2 时, 表示可以从末尾为 x 且出现次数为 1 转移过来
+            dp[i][x][2] = (dp[i - 1][x][2] + dp[i - 1][x][1]) % MOD;
+
+            // 继承非当前奇偶性的答案
+            dp[i][x ^ 1][1] = dp[i - 1][x ^ 1][1];
+            dp[i][x ^ 1][2] = dp[i - 1][x ^ 1][2];
+        }
+
+        return (int) ((dp[n][0][1] + dp[n][0][2] + dp[n][1][1] + dp[n][1][2]) % MOD);
     }
 
     public static void main(String[] args) {
+        assert new Solution().countStableSubsequences(new int[]{2,3,4,2}) == 14;
     }
 
 }
