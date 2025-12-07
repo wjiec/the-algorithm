2921f0a135957b83ec64d720c677c7bccff22ffc
jayson wang <jayson@laboys.org>
2025-12-07 23:15:22 +0800

    daily

--- leetcode-java/src/weekly/w469/C/Solution.java
+++ leetcode-java/src/weekly/w469/C/Solution.java
@@ -1,6 +1,8 @@
 package weekly.w469.C;
 
-import common.PrettyPrinter;
+import ability.Benchmark;
+
+import java.util.Arrays;
 
 /**
  * Q3. Number of ZigZag Arrays I
@@ -22,6 +24,7 @@ import common.PrettyPrinter;
  * A sequence is said to be strictly decreasing if each element is strictly smaller than its previous one (if exists).
  */
 
+@SuppressWarnings("DuplicatedCode")
 public class Solution {
 
     public int zigZagArrays(int n, int l, int r) {
@@ -32,50 +35,135 @@ public class Solution {
         //  - 不存在连续相同的整数
         //  - 不能出现连续三个递增或递减
         //
-        // dp[i][j][k][l] 表示当前构造第 i 位, 最后一位是 j, 且连续递增或递减状态是 k, 且数量是 l
-        int[][][][] dp = new int[n][r][2][3];
-        for (int j = 0; j < r; j++) dp[0][j][INC][1] = dp[0][j][DEC][1] = 1;
+        // 实际就是构造一个递增之后立马递减的序列
+
+        // dp[i][j][k] 表示在 [0, i] 中最后一个位置为 j 且当前构成递增 = 0 或递减 = 1 的方案个数
+        long[][][] dp = new long[n][r][2];
+        // 初始化: 长度为 1 的序列都是满足要求的
+        for (int j = 0; j < r; j++) dp[0][j][INC] = dp[0][j][DEC] = 1;
+
+        // 枚举当前构造的第几位
         for (int i = 1; i < n; i++) {
-            // 枚举当前位填的数字
+            // 枚举当前位的数字
             for (int j = 0; j < r; j++) {
-                // 在递增情况下
-                //  - 如果前一位是 [0, j) 那么当前填 j 会导致递增数量 + 1
-                //  - 如果前一位是 [j, r) 那么当前填 j 会导致递增数量变为 1
-                //
-                // 也就是:
-                //  - dp[i][j][INC][1] = sum(dp[i - 1][j...r-1][INC | DEC][1 | 2])
-                //  - dp[i][j][INC][2] = sum(dp[i - 1][0..j-1][INC | DEC][1])
-                for (int k = j; k < r; k++) {
-                    dp[i][j][INC][1] = (dp[i][j][INC][1] + dp[i - 1][k][INC][1] + dp[i - 1][k][INC][2] + dp[i - 1][k][DEC][1] + dp[i - 1][k][DEC][2]) % MOD;
-                }
-                for (int k = 0; k < j; k++) {
-                    dp[i][j][INC][2] = (dp[i][j][INC][2] + dp[i - 1][k][INC][1] + dp[i - 1][k][DEC][1]) % MOD;
-                }
-                // 在递减情况下
-                //  - 如果前一位是 [0, j] 那么当前填 j 会导致递减数量变为 1
-                //  - 如果前一位是 (j, r) 那么当前填 j 会导致递减数量 + 1
-                //
-                // 也就是:
-                //  - dp[i][j][DEC][1] = sum(dp[i - 1][0...j][INC | DEC][1 | 2])
-                //  - dp[i][j][DEC][2] = sum(dp[i - 1][j+1...r-1][INC | DEC][1])
-                for (int k = 0; k <= j; k++) {
-                    dp[i][j][DEC][1] = (dp[i][j][DEC][1] + dp[i - 1][k][INC][1] + dp[i - 1][k][INC][2] + dp[i - 1][k][DEC][1] + dp[i - 1][k][DEC][2]) % MOD;
-                }
-                for (int k = j + 1; k < r; k++) {
-                    dp[i][j][DEC][2] = (dp[i][j][DEC][2] + dp[i - 1][k][INC][1] + dp[i - 1][k][DEC][1]) % MOD;
-                }
+                // 如果当前位要递增, 那么前一位就是要 < j 且是要递减的情况
+                for (int pj = 0; pj < j; pj++) dp[i][j][INC] = (dp[i][j][INC] + dp[i - 1][pj][DEC]) % MOD;
+                // 如果当前位要递减, 那么前一位就是要 > j 且是要递增的情况
+                for (int pj = j + 1; pj < r; pj++) dp[i][j][DEC] = (dp[i][j][DEC] + dp[i - 1][pj][INC]) % MOD;
             }
         }
 
-        PrettyPrinter.println(dp);
-
+        // 最后一位可以是任何数且可以是递减或递增
         long ans = 0;
-        for (int j = 0; j < r; j++) ans = (ans + dp[n - 1][j][INC][1] + dp[n - 1][j][DEC][1] + dp[n - 1][j][INC][2] + dp[n - 1][j][DEC][2]) % MOD;
-        System.out.println(ans);
+        for (int j = 0; j < r; j++) ans = (ans + dp[n - 1][j][INC] + dp[n - 1][j][DEC]) % MOD;
         return (int) ans;
     }
 
+    private static class Optimization1 {
+        public int zigZagArrays(int n, int l, int r) {
+            final int MOD = 1_000_000_007, INC = 0, DEC = 1;
+            // 把范围 [l, r] 改到 [0, r - l]
+            r = r - l + 1;
+            // 构造一个长度为 n, 元素值在 [l, r] 范围, 并且满足
+            //  - 不存在连续相同的整数
+            //  - 不能出现连续三个递增或递减
+            //
+            // 实际就是构造一个递增之后立马递减的序列
+
+            // dp[i][j][k] 表示在 [0, i] 中最后一个位置为 j 且当前构成递增 = 0 或递减 = 1 的方案个数
+            int[][][] dp = new int[n][r][2];
+            // 初始化: 长度为 1 的序列都是满足要求的
+            for (int j = 0; j < r; j++) dp[0][j][INC] = dp[0][j][DEC] = 1;
+
+            // 枚举当前构造的第几位
+            for (int i = 1; i < n; i++) {
+                int[] incSum = new int[r + 1], decSum = new int[r + 1];
+                for (int j = 0; j < r; j++) {
+                    incSum[j + 1] = (incSum[j] + dp[i - 1][j][INC]) % MOD;
+                    decSum[j + 1] = (decSum[j] + dp[i - 1][j][DEC]) % MOD;
+                }
+
+                // 枚举当前位的数字
+                for (int j = 0; j < r; j++) {
+                    dp[i][j][INC] = decSum[j] % MOD; // \sum{0, 1, ..., j-1}
+                    dp[i][j][DEC] = (incSum[r] - incSum[j + 1] + MOD) % MOD; // \sum{j+1, j+2, ..., r-1}
+                }
+            }
+
+            // 最后一位可以是任何数且可以是递减或递增
+            long ans = 0;
+            for (int j = 0; j < r; j++) ans = (ans + dp[n - 1][j][INC] + dp[n - 1][j][DEC]) % MOD;
+            return (int) ans;
+        }
+    }
+
+    private static class Optimization2 {
+        public int zigZagArrays(int n, int l, int r) {
+            final int MOD = 1_000_000_007; r = r - l + 1;
+
+            // dp[i][j][k] 表示在 [0, i] 中最后一个位置为 j 且当前构成递增 = 0 或递减 = 1 的方案个数
+            int[][] incDp = new int[2][r], decDp = new int[2][r];
+            int[] incSum = new int[r + 1], decSum = new int[r + 1];
+            Arrays.fill(incDp[0], 1); Arrays.fill(decDp[0], 1);
+
+            for (int i = 1; i < n; i++) {
+                int curr = i & 1, prev = curr ^ 1;
+                for (int j = 0; j < r; j++) {
+                    incSum[j + 1] = (incSum[j] + incDp[prev][j]) % MOD;
+                    decSum[j + 1] = (decSum[j] + decDp[prev][j]) % MOD;
+                }
+
+                // 枚举当前位的数字
+                for (int j = 0; j < r; j++) {
+                    incDp[curr][j] = decSum[j]; // 前缀和
+                    decDp[curr][j] = (incSum[r] - incSum[j + 1] + MOD) % MOD; // 后缀和
+                }
+            }
+
+            // 最后一位可以是任何数且可以是递减或递增
+            long ans = 0; int b = (n & 1) ^ 1;
+            for (int j = 0; j < r; j++) ans = (ans + incDp[b][j] + decDp[b][j]) % MOD;
+            return (int) ans;
+        }
+    }
+
+    private static class Optimization3 {
+        public int zigZagArrays(int n, int l, int r) {
+            final int MOD = 1_000_000_007; r = r - l + 1;
+
+            // dp[i][j][k] 表示在 [0, i] 中最后一个位置为 j 且当前构成递增 = 0 或递减 = 1 的方案个数
+            int[] inc = new int[r], dec = new int[r];
+            Arrays.fill(inc, 1); Arrays.fill(dec, 1);
+
+            for (int i = 1; i < n; i++) {
+                int[] incS = new int[r], decS = new int[r];
+                for (int j = 1; j < r; j++) decS[j] = (decS[j - 1] + dec[j - 1]) % MOD;
+                for (int j = r - 2; j >= 0; j--) incS[j] = (incS[j + 1] + inc[j + 1]) % MOD;
+
+                inc = decS; dec = incS;
+            }
+
+            // 最后一位可以是任何数且可以是递减或递增
+            long ans = 0;
+            for (int j = 0; j < r; j++) ans = (ans + inc[j] + dec[j]) % MOD;
+            return (int) ans;
+        }
+    }
+
     public static void main(String[] args) {
+        Benchmark.benchmark("Optimization3", () -> {
+            assert new Optimization3().zigZagArrays(2000, 1, 2000) == 594850306;
+        });
+
+        Benchmark.benchmark("Optimization2", () -> {
+            assert new Optimization2().zigZagArrays(2000, 1, 2000) == 594850306;
+        });
+
+        Benchmark.benchmark("Optimization1", () -> {
+            assert new Optimization1().zigZagArrays(3, 1, 3) == 10;
+            assert new Optimization1().zigZagArrays(3, 4, 5) == 2;
+        });
+
         assert new Solution().zigZagArrays(3, 1, 3) == 10;
         assert new Solution().zigZagArrays(3, 4, 5) == 2;
     }
--- leetcode-java/src/weekly/w469/D/Solution.java
+++ leetcode-java/src/weekly/w469/D/Solution.java
@@ -1,5 +1,7 @@
 package weekly.w469.D;
 
+import common.Tag;
+
 /**
  * Q4. Number of ZigZag Arrays II
  *
@@ -23,11 +25,55 @@ package weekly.w469.D;
 
 public class Solution {
 
+    private static final int MOD = 1_000_000_007;
+
+    @Tag("矩阵快速幂")
     public int zigZagArrays(int n, int l, int r) {
-        return 1;
+        int k = r - l + 1;
+        long[][] mat = new long[2 * k][2 * k];
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < i; j++) mat[i][k + j] = 1;
+            for (int j = i + 1; j < k; j++) mat[k + i][j] = 1;
+        }
+
+        long[][] f1 = new long[2 * k][1];
+        for (int i = 0; i < f1.length; i++) f1[i][0] = 1;
+
+        long[][] fn = powMul(mat, n - 1, f1); long ans = 0;
+        for (var v : fn) ans += v[0];
+        System.out.println(ans);
+        return (int) (ans % MOD);
+    }
+
+    // a ^ k * b
+    private long[][] powMul(long[][] a, int k, long[][] b) {
+        long[][] ans = b;
+        for (; k > 0; k >>= 1) {
+            if ((k & 1) != 0) {
+                ans = mul(a, ans);
+            }
+            a = mul(a, a);
+        }
+        return ans;
+    }
+
+    private long[][] mul(long[][] a, long[][] b) {
+        long[][] ans = new long[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int k = 0; k < a[i].length; k++) {
+                if (a[i][k] == 0) continue;
+
+                for (int j = 0; j < b[k].length; j++) {
+                    ans[i][j] = (ans[i][j] + a[i][k] * b[k][j]) % MOD;
+                }
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
+        assert new Solution().zigZagArrays(3, 4, 5) == 2;
+        assert new Solution().zigZagArrays(3, 1, 3) == 10;
     }
 
 }
