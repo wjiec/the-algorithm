ec62e702700529a748c7eba9bcb43b8780ae4a99
jayson wang <jayson@laboys.org>
2026-01-08 22:04:03 +0800

    daily

--- leetcode-java/src/weekly/w471/C/Solution.java
+++ leetcode-java/src/weekly/w471/C/Solution.java
@@ -31,18 +31,30 @@ public class Solution {
         }
 
         // 如果子串里有两个字符
+        //  - 也就是在一个区间 [l, r) 里, len(a) == len(b)
+        //      - 前缀和里 len(a) == aSum[r + 1] - aSum[l]
+        //      - 前缀和里 len(b) == bSum[r + 1] - bSum[l]
+        //  - 也就是 aSum[r + 1] - aSum[l] = bSum[r + 1] - bSum[l]
+        //      - 也就是 bSum[l] - aSum[l] = bSum[r + 1] - aSum[r + 1]
+        //  - 我们令 s[i] = a[i] - b[i], 并计算 s 的前缀和, 找到前缀和相同的项就构成了一个区间
         ans = Math.max(ans, group2(chars, 'a', 'b'));
         ans = Math.max(ans, group2(chars, 'a', 'c'));
         ans = Math.max(ans, group2(chars, 'b', 'c'));
 
-        // 如果子串里三个字符, 把 (a, b) 压缩一下
-        //  - (a + n) << 32 | (b + n)
+        // 如果子串里三个字符
+        //  - 也就是在一个区间 [l, r) 里, len(a) == len(b) == len(c)
+        //      - 根据传递性, 我们有 len(a) == len(b) 且 len(b) == len(c)
+        //  - 此时我们有：
+        //      - bSum[l] - aSum[l] = bSum[r + 1] - aSum[r + 1]
+        //      - cSum[l] - bSum[l] = cSum[r + 1] - bSum[r + 1]
+        //  - 我们使用一个元组来表示 (bSum[l] - aSum[l], cSum[l] - bSum[l]) == (bSum[r + 1] - aSum[r + 1], cSum[r + 1] - bSum[r + 1])
+        //  - 也就是令 s[i] = (b[i] - a[i], c[i] - b[i]), 找到前缀和 s 中相同的项构成一个区间
         BiFunction<Integer, Integer, Long> comp = (a, b) -> (long) ((long) (a + n) << 32 | (b + n));
         Map<Long, Integer> p = new HashMap<>(); p.put(comp.apply(0, 0), -1);
         for (int i = 0, a = 0, b = 0, c = 0; i < n; i++) {
             switch (chars[i]) { case 'a' -> a++; case 'b' -> b++; case 'c' -> c++; }
 
-            long k = comp.apply(a - b, b - c);
+            long k = comp.apply(b - a, c - b);
             if (p.containsKey(k)) ans = Math.max(ans, i - p.get(k));
             else p.put(k, i);
         }
--- leetcode-java/src/weekly/w471/D/Solution.java
+++ leetcode-java/src/weekly/w471/D/Solution.java
@@ -1,5 +1,11 @@
 package weekly.w471.D;
 
+import common.Tag;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Q4. Sum of Perfect Square Ancestors
  *
@@ -22,10 +28,50 @@ package weekly.w471.D;
  * from node i to the root node 0, excluding i itself.
  */
 
+@SuppressWarnings({"unchecked", "DuplicatedCode"})
 public class Solution {
 
+    private static final int MAX_N = 100_001;
+    private static final int[] core = new int[MAX_N];
+    static {
+        // 对于 v1 = p1^e1 * p2^e2 (对 v 进行质因数分解), v2 = p3^e3 * p4^e4
+        //  - 将其相乘之后得到 (p1^e1 * p2^e2) * (p3^e3 * p4^e4)
+        //  - 我们将其中的所有指数中的 2 提取出来, 得到 (p1^e1' * p2^e2')^2 * (p3^e3' * p4^e4')^2
+        //  - 现在我们只需要保证 (p1^e1' * p2^e2') * (p3^e3' * p4^e4') 为完全平方数即可
+        //      - 也就是 (p1^e1' * p2^e2') == (p3^e3' * p4^e4')
+        // 我们称其为 "平方剩余核 core"
+        for (int i = 1; i < MAX_N; i++) {
+            if (core[i] == 0) { // 当前 i 是一个平方剩余核
+                for (int j = 1; i * j * j < MAX_N; j++) {
+                    core[i * j * j] = i;
+                }
+            }
+        }
+    }
+
+    @Tag({"平方剩余核", "无平方因子核"})
     public long sumOfAncestors(int n, int[][] edges, int[] nums) {
-        return 1;
+        List<Integer>[] g = new List[n];
+        Arrays.setAll(g, i -> new ArrayList<>());
+        for (var edge : edges) {
+            g[edge[0]].add(edge[1]);
+            g[edge[1]].add(edge[0]);
+        }
+
+        return dfs(g, 0, -1, nums);
+    }
+
+    private final int[] freq = new int[MAX_N];
+
+    private long dfs(List<Integer>[] g, int curr, int parent, int[] nums) {
+        int c = core[nums[curr]]; long ans = freq[c]++;
+        for (var next : g[curr]) {
+            if (next != parent) {
+                ans += dfs(g, next, curr, nums);
+            }
+        }
+        freq[c]--; // 回滚
+        return ans;
     }
 
     public static void main(String[] args) {
