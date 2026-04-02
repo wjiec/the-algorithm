7986a3772f8811e1109e844a9082bd6350b7c7bf
jayson wang <jayson@laboys.org>
2026-04-02 19:44:22 +0800

    daily

--- leetcode-java/src/weekly/w477/C/Solution.java
+++ leetcode-java/src/weekly/w477/C/Solution.java
@@ -2,6 +2,8 @@ package weekly.w477.C;
 
 import common.Checker;
 
+import java.util.stream.IntStream;
+
 /**
  * Q3. Concatenate Non-Zero Digits and Multiply by Sum II
  *
@@ -24,11 +26,54 @@ import common.Checker;
 
 public class Solution {
 
+    private static final int MOD = 1_000_000_007;
+    private static final int MAX_N = 100_001;
+    private static final int[] pow10 = new int[MAX_N];
+    static { pow10[0] = 1; }
+    static { for (int i = 1; i < MAX_N; i++) pow10[i] = (int) ((pow10[i - 1] * 10L) % MOD); }
+
     public int[] sumAndMultiply(String s, int[][] queries) {
-        return null;
+        int n = s.length();
+        // 对于 1234... 这个数字, 我们我们已知从 [0, i] 的数字是 v_i
+        //  - 那么对于 [l, i] 的数字我们可以使用 v_i - v_l * 10 ^ (i - l + 1) 的方式得到
+        //      - 也就是例如对于 i = 3, v_i = 1234, 如果我们只需要知道 [2, 3] 的数字的话, 此时 v_l = 12
+        //      - v_i - v_l * 10 ^ (i - l + 1) = 1234 - 12 * 10 ^ 2 = 34
+        //
+        // 如果出现 0 的话, 我们需要将其扣除. 剩下就是用前缀和计算所有的值即可
+        int[] digitSum = new int[n + 1]; // 数字的前缀和
+        int[] preSum = new int[n + 1]; // 数的前缀和
+        int[] nonZeroSum = new int[n + 1]; // 非 0 数字个数前缀和
+        for (int i = 0; i < n; i++) {
+            int digit = s.charAt(i) - '0';
+
+            digitSum[i + 1] = digitSum[i] + digit;
+            preSum[i + 1] = digit == 0 ? preSum[i] : (int) ((preSum[i] * 10L + digit) % MOD);
+            nonZeroSum[i + 1] = nonZeroSum[i] + (digit != 0 ? 1 : 0);
+        }
+
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int l = queries[i][0], r = queries[i][1] + 1;
+            // 计算从 [l, r) 有多少个非零的字符, 也就是计算指数是多少
+            int digits = nonZeroSum[r] - nonZeroSum[l];
+            // 计算 [l, r) 的数是多少, 也就是 v_r - v_l * 10 ^ digits, 且需要保证结果非负
+            long base = preSum[r] - (((long) preSum[l] * pow10[digits]) % MOD) + MOD;
+            // 保存答案
+            ans[i] = (int) ((base * (digitSum[r] - digitSum[l])) % MOD);
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
+        long v = 57569977386369791L;
+        assert Checker.check(new Solution().sumAndMultiply(String.valueOf(v), new int[][]{
+            {0,0},{0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,7},{0,8},{0,9},{0,10},{0,11},{0,12},{0,13},{0,14},{0,15},{0,16}
+        }), IntStream.range(0, 17).map((i) -> {
+            char[] chars = String.valueOf(v).substring(0, i + 1).toCharArray();
+            int sum = 0; for (var c : chars) sum += c - '0';
+            return (int) ((Long.parseLong(new String(chars)) * sum) % 1_000_000_007);
+        }).toArray());
+
         assert Checker.check(new Solution().sumAndMultiply("10203004", new int[][]{{0,7},{1,3},{4,6}}), new int[]{12340, 4, 9});
         assert Checker.check(new Solution().sumAndMultiply("1000", new int[][]{{0,3},{1,1}}), new int[]{1,0});
         assert Checker.check(new Solution().sumAndMultiply("9876543210", new int[][]{{0,9}}), new int[]{444444137});
--- leetcode-java/src/weekly/w477/D/Solution.java
+++ leetcode-java/src/weekly/w477/D/Solution.java
@@ -1,6 +1,6 @@
 package weekly.w477.D;
 
-import java.util.Arrays;
+import common.Tag;
 
 /**
  * Q4. Number of Effective Subsequences
@@ -21,42 +21,46 @@ import java.util.Arrays;
 
 public class Solution {
 
-    public int countEffective(int[] nums) {
-        int max = 0;
-        for (var v : nums) max = Math.max(max, v);
+    private static final int MOD = 1_000_000_007, MAX_N = 1_000_001;
+    private static final int[] pow2 = new int[MAX_N];
+    static { pow2[0] = 1; }
+    static { for (int i = 1; i < MAX_N; i++) pow2[i] = (pow2[i - 1] << 1) % MOD; }
 
-        int bits = 32 - Integer.numberOfLeadingZeros(max);
-        // 完整数组的强度是最大的, 我们需要让剩余数组的强度减少
-        //  - 移除所有在 i 位上置位的数字
-        //  - 或者时移除所有在 i, j, ... 位上置位的数字 (每一个位必须完整移除)
+    @Tag({"高维前缀和", "SOS DP"})
+    public int countEffective(int[] nums) {
+        // 所有数的按位或可以得到一个最大的数 x, 我们需要找到所有子序列的按位或 < x 的数
+        //  - 可以转换成所有子序列的数量 - 按位或等于 x 的子序列数量
+        //      - 所有子序列的数量 = 2 ^ n 个
         //
-        // 由于一个数字可能有多个位被置位, 所以他们之间会有关联
-        //  - 对于包含某一个位的所有数字进行 & 运算即可知道这一个位与哪些位之间存在关联
-        int[] bitset = new int[bits], ands = new int[bits];
-        Arrays.fill(ands, (1 << bits) - 1);
-        for (var v : nums) {
-            for (int i = 0; i < bits; i++) {
-                if ((v & (1 << i)) != 0) {
-                    bitset[i]++; ands[i] &= v;
-                }
+        // 问题变成从数组中找到子序列使得按位或等于 x 的方案有多少
+        //  - "恰好"类型的问题可以转换成"至多"问题
+        //
+        // 将问题转换为从数组中找到子序列使得按位或为 x 的子集
+        //  - 也就是找到数组中的数是 x 的子集的元素, 那么这些数的任意子序列按位或都是 <= x 的
+        int x = 0; for (var v : nums) x |= v;
+
+        // 计算所需的位数以及 dp 数组
+        //  - 计算完成后 dp[s] 就表示在 nums 中有多少个元素是 s 的子集
+        int w = 32 - Integer.numberOfLeadingZeros(x), u = 1 << w;
+        int[] dp = new int[u]; for (var v : nums) dp[v]++;
+        for (int i = 0; i < w; i++) { // 执行状态转移
+            if ((x & (1 << i)) == 0) continue;
+
+            int b = 1 << i;
+            for (int s = b; s < u; s = (s + 1) | b) {
+                dp[s] += dp[s ^ b];
             }
         }
 
-        return 3;
+        // 计算 nums 中有多少个子序列
+        int ans = pow2[nums.length], s = x;
+        // 枚举 x 的子集并去掉这些数
+        do {
+            var sp2 = pow2[dp[s]];
+            ans = (ans + (Integer.bitCount(x ^ s) % 2 == 1 ? 1 : -1) * sp2) % MOD;
+        } while ((s = (s - 1) & x) != x);
+        return (ans + MOD) % MOD;
     }
-
-    // 当前在选择第 i 位, 剩余可选的位是 mask
-    private int dfs(int[] bitset, int[] ands, int i, int mask, int[] remain) {
-        if (mask == 0) return 1;
-        if (bitset[i] == 0) return dfs(bitset, ands, i + 1, mask, remain);
-
-        int ans = 0;
-        for (int j = 0; j < bitset.length; j++) {
-            // 去掉这一位之后, 可能会导致后续的位也被一起删掉
-        }
-        return ans;
-    }
-
 
     public static void main(String[] args) {
         assert new Solution().countEffective(new int[]{1,2,3}) == 3;
