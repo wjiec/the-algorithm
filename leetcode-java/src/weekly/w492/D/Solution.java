433d2ec434c830f8778d80af5cd2e348d7b4ee40
jayson wang <jayson@laboys.org>
2026-05-10 23:23:44 +0800

    daily

--- leetcode-java/src/weekly/w492/D/Solution.java
+++ leetcode-java/src/weekly/w492/D/Solution.java
@@ -4,6 +4,27 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Q4. Minimum Cost to Partition a Binary String
+ *
+ * https://leetcode.cn/contest/weekly-contest-492/problems/minimum-cost-to-partition-a-binary-string/
+ *
+ * You are given a binary string s and two integers encCost and flatCost.
+ *
+ * For each index i, s[i] = '1' indicates that the ith element is sensitive, and s[i] = '0' indicates that it is not.
+ *
+ * The string must be partitioned into segments. Initially, the entire string forms a single segment.
+ *
+ * For a segment of length L containing X sensitive elements:
+ *
+ * If X = 0, the cost is flatCost.
+ * If X > 0, the cost is L * X * encCost.
+ * If a segment has even length, you may split it into two contiguous segments of equal length and
+ * the cost of this split is the sum of costs of the resulting segments.
+ *
+ * Return an integer denoting the minimum possible total cost over all valid partitions.
+ */
+
 public class Solution {
 
     public long minCost(String s, int encCost, int flatCost) {
